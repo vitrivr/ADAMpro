c50876fb16babba3088f70e7ef0ca480970cf891
Ivan Giangreco <ivan.giangreco@unibas.ch>
2015-10-09 17:00:30 +0200

    reformat startup

--- src/main/scala-2.11/ch/unibas/dmi/dbis/adam/main/SparkStartup.scala
+++ src/main/scala-2.11/ch/unibas/dmi/dbis/adam/main/SparkStartup.scala
@@ -2,7 +2,7 @@ package ch.unibas.dmi.dbis.adam.main
 
 import ch.unibas.dmi.dbis.adam.datatypes.bitString.{BitString, MinimalBitString}
 import ch.unibas.dmi.dbis.adam.storage.components.{IndexStorage, TableStorage}
-import ch.unibas.dmi.dbis.adam.storage.engine.{ParquetDataStorage, PostgresDataStorage}
+import ch.unibas.dmi.dbis.adam.storage.engine.{PostgresDataStorage, LevelDBDataStorage, ParquetDataStorage}
 import org.apache.spark.sql.hive.HiveContext
 import org.apache.spark.{SparkConf, SparkContext}
 
@@ -12,19 +12,22 @@ import org.apache.spark.{SparkConf, SparkContext}
  * Ivan Giangreco
  * August 2015
  */
-object  SparkStartup {
-  val sparkConfig = new SparkConf().setAppName("ADAMtwo").setMaster("local[32]")
-  sparkConfig.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-  sparkConfig.set("spark.kryoserializer.buffer.max", "512m");
-  sparkConfig.set("spark.kryoserializer.buffer", "256");
-  sparkConfig.set("spark.driver.maxResultSize", "0");
-  sparkConfig.set("spark.driver.memory", "9g");
-  sparkConfig.set("spark.driver.host", "localhost");
-  sparkConfig.set("spark.rdd.compress", "true");
-  sparkConfig.set("spark.parquet.block.size", (1024 * 1024 * 16).toString)
-  sparkConfig.set("spark.sql.parquet.compression.codec", "gzip")
-  sparkConfig.set("spark.scheduler.mode", "FAIR")
-  sparkConfig.registerKryoClasses(Array(classOf[BitString[_]], classOf[MinimalBitString]))
+object SparkStartup {
+  val sparkConfig = new SparkConf().setAppName("ADAMtwo").setMaster("local[64]")
+    .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+    .set("spark.kryoserializer.buffer.max", "512m")
+    .set("spark.kryoserializer.buffer", "256")
+    .set("spark.driver.maxResultSize", "0")
+    .set("spark.driver.memory", "9g")
+    .set("spark.driver.host", "localhost")
+    .set("spark.rdd.compress", "true")
+    .set("spark.parquet.block.size", (1024 * 1024 * 16).toString)
+    .set("spark.sql.parquet.compression.codec", "gzip")
+    .set("spark.cassandra.connection.host", "192.168.123.10")
+    .set("spark.cassandra.auth.username", "cassandra")
+    .set("spark.cassandra.auth.password", "cassandra")
+    .set("spark.scheduler.allocation.file", "/Users/gianiv01/Development/adamtwo/conf/scheduler.xml")
+    .registerKryoClasses(Array(classOf[BitString[_]], classOf[MinimalBitString]))
 
   val sc = new SparkContext(sparkConfig)
   //val sqlContext = new SQLContext(sc)
@@ -34,6 +37,7 @@ object  SparkStartup {
   sqlContext.setConf("spark.sql.avro.deflate.level", "5")
   sqlContext.setConf("spark.parquet.block.size", (1024 * 1024 * 8).toString)
 
-  val tableStorage: TableStorage = PostgresDataStorage
+  val tableStorage : TableStorage = LevelDBDataStorage
+  val metadataStorage : TableStorage = PostgresDataStorage
   val indexStorage: IndexStorage = ParquetDataStorage
 }
