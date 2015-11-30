b03d85e86f57c85971c4af522493a0fa363732b2
Ivan Giangreco <ivan.giangreco@unibas.ch>
2015-11-30 10:55:47 +0100

    adjustments to spark config

--- src/main/scala-2.10/ch/unibas/dmi/dbis/adam/main/SparkStartup.scala
+++ src/main/scala-2.10/ch/unibas/dmi/dbis/adam/main/SparkStartup.scala
@@ -14,31 +14,17 @@ import org.apache.spark.{SparkConf, SparkContext}
  * August 2015
  */
 object SparkStartup {
-  val sparkConfig = new SparkConf().setAppName("ADAMtwo").setMaster("local[64]")
+  val sparkConfig = new SparkConf().setAppName("ADAMtwo")
     .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
     .set("spark.kryoserializer.buffer.max", "512m")
     .set("spark.kryoserializer.buffer", "256")
     .set("spark.driver.maxResultSize", "0")
     .set("spark.driver.memory", "9g")
-    .set("spark.driver.host", "localhost")
-    .set("spark.rdd.compress", "true")
-    .set("spark.parquet.block.size", (1024 * 1024 * 16).toString)
-    .set("spark.sql.parquet.compression.codec", "gzip")
-    .set("spark.cassandra.connection.host", "192.168.99.100")
-    .set("spark.cassandra.auth.username", "cassandra")
-    .set("spark.cassandra.auth.password", "cassandra")
-    .set("spark.scheduler.allocation.file", "/Users/gianiv01/Development/adamtwo/conf/scheduler.xml")
     .registerKryoClasses(Array(classOf[BitString[_]], classOf[MinimalBitString], classOf[FeatureVectorWrapper]))
 
   val sc = new SparkContext(sparkConfig)
   //val sqlContext = new SQLContext(sc)
   val sqlContext = new HiveContext(sc)
-  sqlContext.setConf("hive.metastore.warehouse.dir", Startup.config.hivePath.toAbsolute.toString())
-
-
-  sqlContext.setConf("spark.sql.avro.compression.codec", "deflate")
-  sqlContext.setConf("spark.sql.avro.deflate.level", "5")
-  sqlContext.setConf("spark.parquet.block.size", (1024 * 1024 * 8).toString)
 
   val featureStorage : FeatureStorage = LevelDBDataStorage
   val metadataStorage : MetadataStorage = PostgresDataStorage
