deb18a0858b20d2dd4541aaad757530a74db2bb8
Ivan Giangreco <ivan.giangreco@unibas.ch>
2015-12-02 10:22:32 +0100

    evaluation performance startup

---
+++ src/main/scala-2.10/ch/unibas/dmi/dbis/adam/evaluation/EvaluationPerformanceStartup.scala
@@ -0,0 +1,20 @@
+package ch.unibas.dmi.dbis.adam.evaluation
+
+import ch.unibas.dmi.dbis.adam.config.AdamConfig
+import ch.unibas.dmi.dbis.adam.main.SparkStartup
+import com.typesafe.config.ConfigFactory
+
+/**
+ * adamtwo
+ *
+ * Ivan Giangreco
+ * December 2015
+ */
+object EvaluationPerformanceStartup {
+    val config: AdamConfig = new AdamConfig(ConfigFactory.load())
+
+    def main(args : Array[String]) {
+      SparkStartup
+      new EvaluationPerformer().start()
+    }
+  }
