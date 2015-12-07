ae976d984a74d74e142ff17d46cf1255b6e126b9
Ivan Giangreco <ivan.giangreco@unibas.ch>
2015-12-07 08:32:49 +0100

    added all evaluatin configs

--- src/main/scala-2.10/ch/unibas/dmi/dbis/adam/evaluation/EvaluationConfig.scala
+++ src/main/scala-2.10/ch/unibas/dmi/dbis/adam/evaluation/EvaluationConfig.scala
@@ -9,7 +9,7 @@ import ch.unibas.dmi.dbis.adam.index.structures.IndexStructures
  * November 2015
  */
 object EvaluationConfig {
-  val dbSizes = Seq(1000, 10000) //, 50000, 100000, 500000, 1000000, 5000000, 10000000, 20000000, 50000000)
+  val dbSizes = Seq(1000, 10000, 50000, 100000, 500000, 1000000, 5000000, 10000000, 20000000, 50000000)
   val vectorSizes = Seq(10, 50, 100, 200, 500)
 
   val indexes = Seq(IndexStructures.LSH, IndexStructures.SH, IndexStructures.VAF, IndexStructures.VAV, IndexStructures.ECP)
