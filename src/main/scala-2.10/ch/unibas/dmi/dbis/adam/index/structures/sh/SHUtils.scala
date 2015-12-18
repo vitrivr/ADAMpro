0d6ae909f08cf88b72b7a9036e741247f2fd05ba
Ivan Giangreco <ivan.giangreco@unibas.ch>
2015-12-18 10:14:10 +0100

    slightly improved performance of SH, more necessary

--- src/main/scala-2.10/ch/unibas/dmi/dbis/adam/index/structures/sh/SHUtils.scala
+++ src/main/scala-2.10/ch/unibas/dmi/dbis/adam/index/structures/sh/SHUtils.scala
@@ -1,10 +1,8 @@
 package ch.unibas.dmi.dbis.adam.index.structures.sh
 
 import breeze.linalg.{*, DenseMatrix}
-import ch.unibas.dmi.dbis.adam.datatypes.feature.Feature
-import Feature._
 import ch.unibas.dmi.dbis.adam.datatypes.bitString.BitString
-import ch.unibas.dmi.dbis.adam.datatypes.feature.Feature
+import ch.unibas.dmi.dbis.adam.datatypes.feature.Feature._
 
 /**
  * adamtwo
@@ -20,6 +18,7 @@ private[sh] object SHUtils {
    * @return
    */
   @inline def hashFeature(f : FeatureVector, indexMetaData : SHIndexMetaData) : BitString[_] = {
+    //TODO: store values!
     val fMat = f.toDenseVector.toDenseMatrix
     val pca = indexMetaData.pca.toDenseMatrix
 
@@ -27,8 +26,7 @@ private[sh] object SHUtils {
 
     val res = {
       val omegai : DenseMatrix[VectorBase] = indexMetaData.omegas(*, ::) :* v
-      omegai :+= conv_double2vectorBase(Math.PI / 2.0)
-      val ys = omegai.map(x => math.sin(x))
+      val ys = omegai.map(x => math.sin(x + (Math.PI / 2.0)))
       val yi = ys(*, ::).map(_.toArray.product).toDenseVector
 
       yi.findAll(x => x > 0)
