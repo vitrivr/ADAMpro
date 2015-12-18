5bf213c432194b4a9a25f80d2990bc824c8b7631
Ivan Giangreco <ivan.giangreco@unibas.ch>
2015-12-18 10:18:15 +0100

    removed deletion of file in parquet as it wouldnt work on Hadoop

--- src/main/scala-2.10/ch/unibas/dmi/dbis/adam/storage/engine/ParquetDataStorage.scala
+++ src/main/scala-2.10/ch/unibas/dmi/dbis/adam/storage/engine/ParquetDataStorage.scala
@@ -1,12 +1,9 @@
 package ch.unibas.dmi.dbis.adam.storage.engine
 
-import java.io.File
-
 import ch.unibas.dmi.dbis.adam.config.AdamConfig
 import ch.unibas.dmi.dbis.adam.index.Index.IndexName
 import ch.unibas.dmi.dbis.adam.main.SparkStartup
 import ch.unibas.dmi.dbis.adam.storage.components.IndexStorage
-import org.apache.commons.io.FileUtils
 import org.apache.spark.sql.{DataFrame, SaveMode}
 
 /**
@@ -18,5 +15,5 @@ import org.apache.spark.sql.{DataFrame, SaveMode}
 object ParquetDataStorage extends IndexStorage {
   override def read(indexname: IndexName) : DataFrame = SparkStartup.sqlContext.read.parquet(AdamConfig.indexPath + "/" + indexname)
   override def write(indexname: IndexName, df: DataFrame): Unit = df.write.mode(SaveMode.Overwrite).parquet(AdamConfig.indexPath + "/" + indexname)
-  override def drop(indexname: IndexName): Unit = FileUtils.deleteQuietly(new File(AdamConfig.indexPath + "/" + indexname))
+  override def drop(indexname: IndexName): Unit = ??? //TODO: hadoop
 }
