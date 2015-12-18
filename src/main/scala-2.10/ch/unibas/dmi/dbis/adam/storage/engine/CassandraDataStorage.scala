38095c84a9fcbebdb4554c6a72706ab8d0615ac7
Ivan Giangreco <ivan.giangreco@unibas.ch>
2015-12-18 10:17:53 +0100

    WHERE IN ... query split if filter too large, saving data TODO added

--- src/main/scala-2.10/ch/unibas/dmi/dbis/adam/storage/engine/CassandraDataStorage.scala
+++ src/main/scala-2.10/ch/unibas/dmi/dbis/adam/storage/engine/CassandraDataStorage.scala
@@ -20,12 +20,22 @@ object CassandraDataStorage extends FeatureStorage with Serializable {
   case class InternalCassandraRowFormat(id: Long, f : Seq[Float])
 
   override def read(entityname: EntityName, filter: Option[scala.collection.Set[TupleID]]): DataFrame = {
-    val cassandraScan = if(filter.isDefined){
-      SparkStartup.sc.cassandraTable("adamtwo", entityname).where("id IN ?", filter.get)
+    val rowRDD = if(filter.isDefined){
+      val subresults = filter.get.grouped(500).map( subfilter =>
+        SparkStartup.sc.cassandraTable("adamtwo", entityname).where("id IN ?", subfilter).map(crow => Row(crow.getLong(0), asWorkingVectorWrapper(crow.getList[Float](1))))
+      ).toSeq
+
+      var base = subresults(0)
+
+      subresults.slice(1, subresults.length).foreach{ sr =>
+        base = base.union(sr)
+      }
+
+      base
     } else {
-      SparkStartup.sc.cassandraTable("adamtwo", entityname)
+      val cassandraScan = SparkStartup.sc.cassandraTable("adamtwo", entityname)
+      cassandraScan.map(crow => Row(crow.getLong(0), asWorkingVectorWrapper(crow.getList[Float](1))))
     }
-    val rowRDD = cassandraScan.map(crow => Row(crow.getLong(0), asWorkingVectorWrapper(crow.getList[Float](1))))
 
 
     val schema = StructType(
@@ -40,9 +50,7 @@ object CassandraDataStorage extends FeatureStorage with Serializable {
   override def drop(entityname: EntityName): Unit = ??? //TODO
 
   override def write(entityname: EntityName, df: DataFrame, mode: SaveMode): Unit = {
-    if(df.count() == 0){ //TODO: correct this!
-      return
-    }
+    //TODO: save as cassandra, save to cassandra
     df.rdd.map(r => InternalCassandraRowFormat(r.getLong(0).toInt, r.getAs[FeatureVectorWrapper](1).getSeq())).saveToCassandra("adamtwo", entityname)
   }
 
