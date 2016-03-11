1a9c5c83c7f562493d0c55e9a348fdd64c2786a8
Ivan Giangreco <ivan.giangreco@unibas.ch>
2016-03-11 16:06:14 +0100

    adjusted names for Cassandra config

--- src/main/scala-2.10/ch/unibas/dmi/dbis/adam/config/FieldNames.scala
+++ src/main/scala-2.10/ch/unibas/dmi/dbis/adam/config/FieldNames.scala
@@ -7,9 +7,9 @@ package ch.unibas.dmi.dbis.adam.config
   * March 2016
   */
 object FieldNames {
-  val idColumnName = "__ADAMTWO_TID"
-  val internFeatureColumnName = "__ADAMTWO_FEATURES"
-  val distanceColumnName = "__ADAMTWO_DISTANCE"
+  val idColumnName = "adamtwoid"
+  val internFeatureColumnName = "adamtwofeatures"
+  val distanceColumnName = "adamtwodistance"
 
   val featureColumnName = "feature"
 }
