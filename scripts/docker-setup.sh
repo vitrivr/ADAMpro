dfe78a7fe71b0f22bbdf72dfa6fe9cb2d8849b27
Ivan Giangreco <ivan.giangreco@unibas.ch>
2016-04-25 12:18:52 +0200

    use spark container with java 1.8

--- scripts/docker-setup.sh
+++ scripts/docker-setup.sh
@@ -12,6 +12,11 @@ docker network create --driver bridge adampronw
 docker run --net=adampronw -p 5432:5432 -h postgresql --name postgresql --net-alias postgresql -d orchardup/postgresql:latest
 
 # creates Spark container
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ".." && pwd )"
+if [[ "$(docker images -q sparkj1.8 2> /dev/null)" == "" ]]; then
+  docker build -t sparkj1.8 $DIR/scripts
+fi
+
 # note that we mount the target folder to the docker VM
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ".." && pwd )"
-docker run --net=adampronw -d -p 8088:8088 -p 8042:8042 -p 4040:4040 -p 5890:5890 -v $DIR/target:/target -h spark --name spark --net-alias spark sequenceiq/spark:v1.6.0onHadoop2.6.0 -d
+docker run --net=adampronw -d -p 8088:8088 -p 8042:8042 -p 4040:4040 -p 5890:5890 -v $DIR/target:/target -h spark --name spark --net-alias spark sparkj1.8 -d
