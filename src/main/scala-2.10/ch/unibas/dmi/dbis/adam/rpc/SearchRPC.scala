153853b5c41ff83c869d3e78066062986561881a
Ivan Giangreco <ivan.giangreco@unibas.ch>
2016-05-10 12:54:36 +0200

    pk field is now returned in metadata and therefore no longer needed as separate field in message

--- src/main/scala-2.10/ch/unibas/dmi/dbis/adam/rpc/SearchRPC.scala
+++ src/main/scala-2.10/ch/unibas/dmi/dbis/adam/rpc/SearchRPC.scala
@@ -13,7 +13,7 @@ import ch.unibas.dmi.dbis.adam.query.handler.{QueryHandler, QueryHints}
 import ch.unibas.dmi.dbis.adam.query.progressive.{QueryHintsProgressivePathChooser, SimpleProgressivePathChooser}
 import io.grpc.stub.StreamObserver
 import org.apache.log4j.Logger
-import org.apache.spark.sql.{DataFrame, types}
+import org.apache.spark.sql.DataFrame
 
 import scala.collection.mutable.ListBuffer
 import scala.concurrent.Future
@@ -290,19 +290,16 @@ class SearchRPC(implicit ac: AdamContext) extends AdamSearchGrpc.AdamSearch {
     val keys = array(cols.map(lit): _*)
     val values = array(cols.map(col): _*)
 
-    val pk = df.schema.fields.filter(_.dataType == types.LongType).head.name
-
     val results = if (!cols.isEmpty) {
       df.withColumn("metadata", asMap(keys, values))
-        .collect().map(row => QueryResultMessage(
-        row.getAs[Long](pk),
+        .collect().map(row =>
+        QueryResultMessage(
         row.getAs[Float](FieldNames.distanceColumnName),
         row.getAs[Map[String, String]]("metadata").toMap
       ))
     } else {
       df
         .collect().map(row => QueryResultMessage(
-        row.getAs[Long](pk),
         row.getAs[Float](FieldNames.distanceColumnName),
         Map[String, String]()
       ))
