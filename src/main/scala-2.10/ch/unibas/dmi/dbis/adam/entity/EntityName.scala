90627acfc14eb35939e5d23d0fa0f98d1cda2bc6
Ivan Giangreco <ivan.giangreco@unibas.ch>
2016-05-12 14:29:19 +0200

    bug in entitynameholder, escaping [...] was wrong and would therefore not replace anything

--- src/main/scala-2.10/ch/unibas/dmi/dbis/adam/entity/EntityName.scala
+++ src/main/scala-2.10/ch/unibas/dmi/dbis/adam/entity/EntityName.scala
@@ -9,13 +9,13 @@ import ch.unibas.dmi.dbis.adam.entity.Entity.EntityName
   * April 2016
   */
 case class EntityNameHolder(originalName: String) {
-  override def toString() = cleanName(originalName)
+  override def toString = cleanName(originalName)
 
   override def canEqual(a: Any) = a.isInstanceOf[EntityNameHolder] || a.isInstanceOf[String]
 
   override def equals(that: Any): Boolean =
     that match {
-      case that: EntityNameHolder => that.canEqual(this) && toString.equals(that.toString())
+      case that: EntityNameHolder => that.canEqual(this) && toString.equals(that.toString)
       case that : String => that.canEqual(this) && originalName.equals(cleanName(that))
       case _ => false
     }
@@ -27,11 +27,11 @@ case class EntityNameHolder(originalName: String) {
     * @param str
     * @return
     */
-  private def cleanName(str : String): String = str.replaceAll("/[^A-Za-z ]/", "").toLowerCase()
+  private def cleanName(str : String): String = str.replaceAll("[^A-Za-z_-]", "").toLowerCase()
 }
 
 object EntityNameHolder {
-  implicit def EntityName2String(name: EntityName): String = name.toString()
+  implicit def EntityName2String(name: EntityName): String = name.toString
 
   implicit def EntityName2String(str: String): EntityName = EntityNameHolder(str)
 }
