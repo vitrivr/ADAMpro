8a85e3b70cb387e519e0cb0a99eb4f1c70a1bb17
Ivan Giangreco <ivan.giangreco@unibas.ch>
2016-05-30 15:21:20 +0200

    switching back to local[4] instead of yarn, as for yarn it would require to properly set up a cluster and configure all params; the runADAM script is for debugging and can therefore just use local

--- scripts/docker-setup.sh
+++ scripts/docker-setup.sh
@@ -19,6 +19,6 @@ if [[ "$(docker images -q spark:j1.8-s1.6.1-h2.6> /dev/null)" == "" ]]; then
   docker build -t sparkj1.8 $DIR/scripts/docker
 fi
 
-cp -R $DIR/conf $DIR/target/conf
-rm $DIR/target/application.conf
-docker run -d -e "SPARK_CONF_DIR=/target/conf" --net=adampronw -p 8088:8088 -p 8042:8042 -p 4040:4040 -p 5890:5890 -v $DIR/target:/target -h spark --name spark --net-alias spark sparkj1.8 -d
+cp -R $DIR/conf $DIR/target/
+rm $DIR/target/conf/application.conf
+docker run -d -e "SPARK_CONF_DIR=/target/conf" --net=adampronw -p 4040:4040 -p 8088:8088 -p 8042:8042 -p 5890:5890 -v $DIR/target:/target -h spark --name spark --net-alias spark sparkj1.8 -d
