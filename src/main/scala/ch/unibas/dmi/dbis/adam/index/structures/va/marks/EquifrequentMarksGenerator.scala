d96a39ffa466cb2a6c38e5a168526cfb3bbbbc57
Ivan Giangreco <ivan.giangreco@unibas.ch>
2016-06-01 08:55:31 +0200

    improved performance of VA file (equifrequent) by replacing folding code

--- src/main/scala/ch/unibas/dmi/dbis/adam/index/structures/va/marks/EquifrequentMarksGenerator.scala
+++ src/main/scala/ch/unibas/dmi/dbis/adam/index/structures/va/marks/EquifrequentMarksGenerator.scala
@@ -6,24 +6,23 @@ import ch.unibas.dmi.dbis.adam.index.IndexingTaskTuple
 import ch.unibas.dmi.dbis.adam.index.structures.va.VAIndex.Marks
 import org.apache.log4j.Logger
 
-import scala.collection.IterableLike
 import scala.collection.mutable.ListBuffer
 
 /**
- * 
- */
+  *
+  */
 private[va] object EquifrequentMarksGenerator extends MarksGenerator with Serializable {
   @transient lazy val log = Logger.getLogger(getClass.getName)
 
-  val distanceSamples = 500
+  val distanceSamples = 10000
 
   /**
-   *
-   * @param samples
-   * @param maxMarks
-   * @return
-   */
-  private[va] def getMarks(samples : Array[IndexingTaskTuple[_]], maxMarks : Seq[Int]) : Marks = {
+    *
+    * @param samples
+    * @param maxMarks
+    * @return
+    */
+  private[va] def getMarks(samples: Array[IndexingTaskTuple[_]], maxMarks: Seq[Int]): Marks = {
     log.debug("get equifrequent marks for VA-File")
 
     val sampleSize = samples.length
@@ -33,143 +32,89 @@ private[va] object EquifrequentMarksGenerator extends MarksGenerator with Serial
 
     val dimensionality = min.length
 
-    val result = (0 until dimensionality).map(dim => Distribution(min(dim), max(dim), distanceSamples))
+    val dimData = (0 until dimensionality).map(dim => Distribution(min(dim), max(dim), distanceSamples))
 
     samples.foreach { sample =>
       var i = 0
-      while (i < dimensionality){
-        result(i).add(sample.feature(i))
+      while (i < dimensionality) {
+        dimData(i).add(sample.feature(i))
         i += 1
       }
     }
 
     (0 until dimensionality).map({ dim =>
-      val counts = result(dim).getCounts(maxMarks(dim))
+      val hist = dimData(dim).histogram
 
-      val interpolated = counts.map(_.toFloat).map(_ * (max(dim) - min(dim)) / sampleSize.toFloat + min(dim))
+      var marks = new Array[Float](maxMarks(dim))
+      var k = 0
+      var sum = 0
+      for (j <- 1 until (maxMarks(dim) - 1)) {
+        var n = (sampleSize - sum) / (maxMarks(dim) - j)
+        while ((j % 2 == 1 && n > 0) || (j % 2 == 0 && n > hist(k))) {
+          sum += hist(k)
+          n -= hist(k)
+          k += 1
+        }
 
-      min(dim) +: interpolated :+ max(dim)
+        marks(j) = min(dim) + k.toFloat * (max(dim) - min(dim)) / distanceSamples.toFloat
+      }
+
+      marks(0) = min(dim)
+      marks(maxMarks(dim) - 1) = max(dim)
+
+      marks.toSeq
     })
   }
 
   /**
-   *
-   * @param data
-   * @return
-   */
-  private def getMin(data : Array[FeatureVector]) : FeatureVector = {
+    *
+    * @param data
+    * @return
+    */
+  private def getMin(data: Array[FeatureVector]): FeatureVector = {
     val dimensionality = data.head.size
-    val base : FeatureVector = Seq.fill(dimensionality)(Float.MaxValue)
+    val base: FeatureVector = Seq.fill(dimensionality)(Float.MaxValue)
 
-    data.foldLeft(base)((baseV, newV) =>  min(baseV, newV))
+    data.foldLeft(base)((baseV, newV) => min(baseV, newV))
   }
 
   /**
-   *
-   * @param data
-   * @return
-   */
-  private def getMax(data : Array[FeatureVector]) : FeatureVector = {
+    *
+    * @param data
+    * @return
+    */
+  private def getMax(data: Array[FeatureVector]): FeatureVector = {
     val dimensionality = data.head.size
-    val base : FeatureVector = Seq.fill(dimensionality)(Float.MinValue)
+    val base: FeatureVector = Seq.fill(dimensionality)(Float.MinValue)
 
-    data.foldLeft(base)((baseV, newV) =>  max(baseV, newV))
+    data.foldLeft(base)((baseV, newV) => max(baseV, newV))
   }
 
   /**
-   *
-   * @param min
-   * @param max
-   * @param sampling_frequency
-   */
+    *
+    * @param min
+    * @param max
+    * @param sampling_frequency
+    */
   private case class Distribution(min: VectorBase, max: VectorBase, sampling_frequency: Int) {
-    val binWidth = (max - min) / sampling_frequency
-    val bounds = (1 to sampling_frequency).map(x => min + binWidth * x).toList
     val data = new ListBuffer[VectorBase]()
 
     /**
-     *
-     * @param item
-     */
-    def add(item : VectorBase): Unit = data += item
+      *
+      * @param item
+      */
+    def add(item: VectorBase): Unit = data += item
 
     /**
-     *
-     * @return
-     */
-    def getHistogram = buildHistogram(bounds, data.toList)
+      *
+      * @return
+      */
+    def histogram: IndexedSeq[Int] = {
+      val mapped = data
+        .map(x => (((x - min) / (max - min)) * sampling_frequency).floor.toInt)
+        .groupBy(x => x).map { case (key, value) => (key, value.size) }
 
-
-    /**
-     *
-     * @param bounds
-     * @param data
-     * @return
-     */
-    private def buildHistogram(bounds: List[VectorBase], data: List[VectorBase]): List[List[VectorBase]] = {
-      bounds match {
-        case h :: Nil =>
-          List(data)
-        case h :: t =>
-          val (l, r) = data.partition(_ <= h); l :: buildHistogram(t, r)
-        case Nil => List(data)
-      }
-    }
-
-    /**
-     *
-     * @param maxMarks
-     * @return
-     */
-    def getCounts(maxMarks: Int) = {
-      val sampleSize = data.length
-      val hist = getHistogram
-
-      (1 until (maxMarks - 1)).map { j =>
-        val nppart = sampleSize * j / (maxMarks - 1)
-        val countSum = hist.foldLeftWhileCounting(0.toLong)(_ <= nppart) { case (acc, bucket) => acc + bucket.length }
-        countSum._2
-      }
+      (0 until sampling_frequency).map(i => mapped.getOrElse(i, 0))
     }
   }
-
-
-
-  implicit class IterableLikeExtension[A, Repr <: IterableLike[A, Repr]](val iterableLike: IterableLike[A, Repr]) extends AnyVal {
-
-    /**
-     *
-     */
-    def foldLeftWhile[B](z: B)(p: B => Boolean)(op: (B, A) => B): B = {
-      var result = z
-      val it = iterableLike.iterator
-      while (it.hasNext && p(result)) {
-        val next = it.next()
-        result = op(result, next)
-      }
-      result
-    }
-
-    /**
-     *
-     */
-    def foldLeftWhileCounting[B](z: B)(p: B => Boolean)(op: (B, A) => B): (Long, B) = {
-      var result = z
-      var i: Long = 0
-      val it = iterableLike.iterator
-      while (it.hasNext) {
-        val next = it.next()
-        result = op(result, next)
-
-        if (!p(result)) {
-          return (i, result)
-        } else {
-          i += 1
-        }
-      }
-
-      return (i, result)
-    }
-  }
-
 }
