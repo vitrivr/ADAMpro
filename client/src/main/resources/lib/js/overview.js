a26aa3bb02a658a05656127e211d3d48532d6043
Ivan Giangreco <ivan.giangreco@unibas.ch>
2016-06-02 14:52:11 +0200

    made overview a bit nicer

--- client/src/main/resources/lib/js/overview.js
+++ client/src/main/resources/lib/js/overview.js
@@ -8,24 +8,42 @@ $(document).ready(function () {
             if (data.code === 200) {
                 jQuery.each(data.entities, function (index, value) {
                     var innerhtml = '';
-                    innerhtml += "<li id='" + value + "' data-entityname='" + value + "' class='collection-item avatar' '>";
-                    innerhtml += "<i class='material-icons circle'>view_list</i>";
-                    innerhtml += "<span class='title'>" + value + "</span>";
+                    innerhtml += "<li id='" + value + "' data-entityname='" + value + "' class='collection-item'" + ">";
+                    innerhtml += "<span class='title' style='font-size: 16px;'>" + value + "</span>";
+                    innerhtml += "<div class='secondary-content green-text'>";
+                    innerhtml += "<a href='#!' class='btndelete'><i class='material-icons'>delete</i></a>";
+                    innerhtml += "<a href='#!' class='btninfo'><i class='material-icons'>info</i></a>";
+                    innerhtml += "</div>";
                     innerhtml += "<div class='details' style='display:none'>";
-                    innerhtml += "<table class='striped'>";
-                    innerhtml += "<thead><tr><th>option</th><th>value</th></tr></thead>";
+                    innerhtml += "<table>";
                     innerhtml += "<tbody>";
                     innerhtml += "</tbody>";
                     innerhtml += "</table>";
                     innerhtml += "</div>";
-                    innerhtml += "<div class='secondary-content'>";
-                    innerhtml += "<a href='#!' class='btndelete'><i class='material-icons'>delete</i></a>";
-                    innerhtml += "<a href='#!' class='btninfo'><i class='material-icons'>info</i></a>";
-                    innerhtml += "</div>";
                     innerhtml += "</li>";
                     $("#info").append(innerhtml);
 
-                    var handler = function () {
+                    var dropHandler = function(){
+                        $("#progress").show();
+                        $.ajax("/entity/drop?entityname=" + value, {
+                            data: "",
+                            contentType: 'application/json',
+                            type: 'GET',
+                            success: function (data) {
+                                if (data.code === 200) {
+                                    $("#" + value).hide();
+                                    showAlert("Entity dropped");
+                                    $("#progress").hide();
+                                } else {
+                                    $("#progress").hide();
+                                    showAlert("Error in request: " + data.message);
+                                }
+                            }
+                        });
+                    }
+
+
+                    var infoHandler = function () {
                         $('#' + value + ' > div.details').show();
                         $("#progress").show();
                         $.ajax("/entity/details?entityname=" + value, {
@@ -37,17 +55,17 @@ $(document).ready(function () {
                                     var innerinnerhtml = '';
                                     jQuery.each(data.details, function (detailsindex, detailsvalue) {
                                         innerinnerhtml += "<tr>";
-                                        innerinnerhtml += "<td>";
+                                        innerinnerhtml += "<td style='font-weight: bold; font-size: 12px; padding: 0px 0px;' width='150px'>";
                                         innerinnerhtml += detailsindex;
                                         innerinnerhtml += "</td>";
-                                        innerinnerhtml += "<td>";
+                                        innerinnerhtml += "<td style='font-size: 12px; padding: 0px 0px;'>";
                                         innerinnerhtml += detailsvalue;
                                         innerinnerhtml += "</td>";
                                         innerinnerhtml += "</tr>";
                                     });
 
                                     $('#' + value + ' > div > table > tbody').html(innerinnerhtml);
-                                    $('#' + value + ' > div.collapsible-header').unbind("click", handler);
+                                    $('#' + value + ' > div.collapsible-header').unbind("click", infoHandler);
                                     $("#progress").hide()
                                 } else {
                                     $("#progress").hide()
@@ -57,12 +75,8 @@ $(document).ready(function () {
                         });
                     };
 
-                    $('#' + value + ' > div > a.btndelete').bind("click", function(){showAlert("Not implemented yet.")});
-                    $('#' + value + ' > div > a.btninfo').bind("click", handler);
-
-                    $('.collapsible').collapsible({
-                        accordion: false
-                    });
+                    $('#' + value + ' > div > a.btndelete').bind("click", dropHandler);
+                    $('#' + value + ' > div > a.btninfo').bind("click", infoHandler);
                 });
 
             } else {
