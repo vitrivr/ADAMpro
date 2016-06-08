d21b1ee7df0ceb60dc961ea0436996f8c0a9fbcd
Ivan Giangreco <ivan.giangreco@unibas.ch>
2016-06-08 08:40:15 +0200

    added stub for evaluation

---
+++ evaluation/src/main/scala/ch/unibas/dmi/dbis/adam/evaluation/main/EvaluationStartup.scala
@@ -0,0 +1,18 @@
+package ch.unibas.dmi.dbis.adam.evaluation.main
+
+import ch.unibas.dmi.dbis.adam.evaluation.grpc.RPCClient
+
+/**
+  * ADAMpro
+  *
+  * Ivan Giangreco
+  * June 2016
+  */
+object EvaluationStartup {
+  val grpcHost = "localhost"
+  val grpcPort = 5890
+
+  def main(args: Array[String]): Unit = {
+    val grpc = RPCClient(grpcHost, grpcPort)
+  }
+}
