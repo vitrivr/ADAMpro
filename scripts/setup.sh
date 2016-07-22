653563ff5d3c6b5803e51fcf04fed137143bb437
Silvan Heller <silvan.heller@unibas.ch>
2016-07-22 11:26:50 +0200

    Moved Removing of containers to specific build-commands and rebuild of base-command. This allows for usage of the setup-script with both worker and submit

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,7 +4,7 @@
 ## -b rebuilds the spark-base image
 ## -r is either WORKER or MASTER or SUBMIT
 ## -s and -h are in format ip:port and are used for the spark master and the hadoop namenode. use :7077 and :9000 please :)
-## TODO Find better handling than getopts which only takes one char (Seriously?)
+## TODO Find better handling than getopts which only takes one char
 
 ## This script assumes that in the folder where scripts/ is placed, you want to have a folder target/ where the conf-folder and the
 
@@ -17,30 +17,13 @@ echo "Installing and updating done, cleaning existing images"
 ##########################
 # Cleaning existing images
 ##########################
-sudo rm -r target/
-mkdir target/
-
-#TODO Maybe not all should be deleted?
-
-sudo docker stop spark-master
-sudo docker rm spark-master
-sudo docker rmi adampar/spark-master:1.6.2-hadoop2.6
-
-sudo docker stop spark-submit
-sudo docker rm spark-submit
-sudo docker rmi adampar/spark-submit:1.6.2-hadoop2.6
-
-sudo docker stop spark-worker
-sudo docker rm spark-worker
-sudo docker rmi adampar/spark-worker:1.6.2-hadoop2.6
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ".." && pwd )"
 
 echo "Parsing hadoop- and spark-vars"
 
 #############################################
 ## Create Core-site.xml and SPARK_MASTER before building containers
-## TODO If this is the way to go, how do we support multiple roles?
+## TODO If this is the way to go, how do we support multiple roles in one script
 #############################################
 while getopts ':h:s:r:b' option; do
   case $option in
@@ -71,8 +54,6 @@ done
 #####################
 # Extract IP and Port
 #####################
-echo "Spark master should be set: $SPARK_MASTER, Hadoop Namenode should be set: $HADOOP_NAMENODE"
-#TODO
 IFS=':' read -r -a array <<< "$SPARK_MASTER"
 SPARK_MASTER_IP=${array[0]}
 SPARK_MASTER_PORT=${array[1]}
@@ -81,18 +62,33 @@ echo "Building Containers"
 
 if [[ $BASE == "yes" ]]; then
     echo "rebuilding base container"
+    sudo docker stop spark-master
+    sudo docker rm spark-master
+    sudo docker rmi adampar/spark-master:1.6.2-hadoop2.6
+
+    sudo docker stop spark-worker
+    sudo docker rm spark-worker
+    sudo docker rmi adampar/spark-worker:1.6.2-hadoop2.6
+
+    sudo docker stop spark-submit
+    sudo docker rm spark-submit
+    sudo docker rmi adampar/spark-submit:1.6.2-hadoop2.6
+
     sudo docker rmi adampar/spark-base:1.6.2-hadoop2.6
     sudo docker build -t adampar/spark-base:1.6.2-hadoop2.6 $DIR/scripts/docker-spark/base/
 fi
 
 ## Builds Containers. The actual method call is below
-## TODO Free ports based on MASTER_PORT
+## TODO port based on MASTER_PORT in the run -p flags
 buildContainer() {
-    echo "Container Building: $1"
+    echo "Building Container: $1"
     ROLE=$1
 
     if [ $ROLE == "MASTER" ]; then
-        echo "building master containers, $SPARK_MASTER_IP:$SPARK_MASTER_PORT, $HADOOP_NAMENODE"
+        sudo docker stop spark-master
+        sudo docker rm spark-master
+        sudo docker rmi adampar/spark-master:1.6.2-hadoop2.6
+
         #postgresql
         sudo docker stop postgresql
         sudo docker rm postgresql
@@ -100,6 +96,7 @@ buildContainer() {
 
         # adampro
         sudo docker build -t adampar/spark-master:1.6.2-hadoop2.6 $DIR/scripts/docker-spark/master
+
         ##9000 is for HDFS
         # 8080, 7077 and 6066 are to listen to spark-submit / spark GUI
         # #8088, 8042 are legacy from the old code TODO are those necessary?
@@ -113,7 +110,12 @@ buildContainer() {
     fi
 
     if [ $ROLE == "WORKER" ]; then
+        sudo docker stop spark-worker
+        sudo docker rm spark-worker
+        sudo docker rmi adampar/spark-worker:1.6.2-hadoop2.6
+
         sudo docker build -t adampar/spark-worker:1.6.2-hadoop2.6 $DIR/scripts/docker-spark/worker
+
         #Ports: 8081 is the UI, 9000 the HDFS Port -> TODO Scale this based on Input
         # 7077, 6066 are for jobs, 4040 for the Job UI
         # 8088 and 8042 are legacy ports TODO
@@ -126,7 +128,10 @@ buildContainer() {
     fi
 
     if [ $ROLE == "SUBMIT" ]; then
-        echo "building submit containers"
+        sudo docker stop spark-submit
+        sudo docker rm spark-submit
+        sudo docker rmi adampar/spark-submit:1.6.2-hadoop2.6
+
         sudo docker build -t adampar/spark-submit:1.6.2-hadoop2.6 $DIR/scripts/docker-spark/submit
 
         # TODO Try which ports are necessary
