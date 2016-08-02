03937c6e8bdba7ab03d0ec3e46789e247370c1df
Ivan Giangreco <ivan.giangreco@unibas.ch>
2016-08-02 14:40:13 +0200

    added comments

--- src/main/scala/ch/unibas/dmi/dbis/adam/index/structures/lsh/signature/LSHSignatureGenerator.scala
+++ src/main/scala/ch/unibas/dmi/dbis/adam/index/structures/lsh/signature/LSHSignatureGenerator.scala
@@ -14,14 +14,29 @@ import ch.unibas.dmi.dbis.adam.index.structures.va.signature.FixedSignatureGener
 class LSHSignatureGenerator(hashTables : Seq[Hasher], m : Int) extends Serializable {
   val signatureGenerator = new FixedSignatureGenerator(hashTables.length, math.ceil(math.log(m) / math.log(2)).toInt)
 
+  /**
+    *
+    * @param f
+    * @return
+    */
   def toSignature(f: FeatureVector) : BitString[_] = {
     signatureGenerator.toSignature(toBuckets(f))
   }
 
+  /**
+    *
+    * @param f
+    * @return
+    */
   def toBuckets(f: FeatureVector) = {
     hashTables.map(ht => ht(f,m))
   }
 
+  /**
+    *
+    * @param b
+    * @return
+    */
   def toBuckets(b : BitString[_]) = {
     signatureGenerator.toCells(b)
   }
