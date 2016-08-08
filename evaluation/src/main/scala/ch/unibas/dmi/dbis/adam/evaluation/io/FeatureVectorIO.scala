2aa0aa2926566cc163937e8246039d9aef96c299
Silvan Heller <silvan.heller@unibas.ch>
2016-08-08 15:50:54 +0200

    Sanity-checks for IO-Classes

--- evaluation/src/main/scala/ch/unibas/dmi/dbis/adam/evaluation/io/FeatureVectorIO.scala
+++ evaluation/src/main/scala/ch/unibas/dmi/dbis/adam/evaluation/io/FeatureVectorIO.scala
@@ -2,8 +2,6 @@ package ch.unibas.dmi.dbis.adam.evaluation.io
 
 import java.io._
 
-import ch.unibas.dmi.dbis.adam.http.grpc.DenseVectorMessage
-
 import scala.collection.mutable.ListBuffer
 import scala.util.Random
 
@@ -16,9 +14,9 @@ object FeatureVectorIO extends App {
 
   //Testing
   val data = IndexedSeq.tabulate(10)(_ => IndexedSeq.fill(20)(Random.nextFloat()))
-  FeatureVectorIO.toFile(new File("resources/test.qv"), data)
+  FeatureVectorIO.writeToFile(new File("resources/test.qv"), data)
   val read = FeatureVectorIO.fromFile(new File("resources/test.qv"))
-  if(data!=read){
+  if (data != read) {
     System.err.println("fail")
   } else System.out.println("Heureka! ")
 
@@ -29,23 +27,24 @@ object FeatureVectorIO extends App {
   /**
     * Reads Float-Seqs from a File
     * Lines are read one-by one
+    *
     * @param delimiter default is , without space
     * @return
     */
-  def fromFile(file: File, delimiter:String=",") : IndexedSeq[IndexedSeq[Float]] = {
+  def fromFile(file: File, delimiter: String = ","): IndexedSeq[IndexedSeq[Float]] = {
     val lb = ListBuffer[IndexedSeq[Float]]()
-    var br : BufferedReader = null
-    try{
+    var br: BufferedReader = null
+    try {
       br = new BufferedReader(new FileReader(file))
       var line = br.readLine()
-      while(line!=null){
+      while (line != null) {
         val q = line.split(delimiter).map(f => f.toFloat)
-        if(line!=""){
-          lb+=q
+        if (line != "") {
+          lb += q
         }
         line = br.readLine()
       }
-    }finally br.close
+    } finally br.close()
 
     lb.toIndexedSeq
   }
@@ -53,26 +52,23 @@ object FeatureVectorIO extends App {
   /**
     * Writes your list of float-seqs to a file. It is guaranteed that the seqs are stored in the order that they are entered
     * since we use a while-loop and data is an indexedSeq
-    * @param data
     */
-  def toFile(file: File, data: IndexedSeq[IndexedSeq[Float]]) : Unit = {
-    var bw : BufferedWriter = null
-    if(!file.exists()){
-      if (file.getParentFile() != null) {
-        file.getParentFile().mkdirs()
-      }
-      file.createNewFile()
-      System.out.println("Created File: "+file.getName)
+  def writeToFile(file: File, data: IndexedSeq[IndexedSeq[Float]]): Unit = {
+    var bw: BufferedWriter = null
+    if (file.getParentFile != null) {
+      file.getParentFile.mkdirs()
     }
-    try{
+    file.delete()
+    file.createNewFile()
+    try {
       bw = new BufferedWriter(new FileWriter(file))
       var counter = 0
-      while(counter<data.size){
+      while (counter < data.size) {
         bw.write(data(counter).mkString(","))
         bw.newLine()
-        counter+=1
+        counter += 1
       }
       bw.flush()
-    }finally bw.close
+    } finally bw.close()
   }
 }
--- evaluation/src/main/scala/ch/unibas/dmi/dbis/adam/evaluation/io/QueryResultIO.scala
+++ evaluation/src/main/scala/ch/unibas/dmi/dbis/adam/evaluation/io/QueryResultIO.scala
@@ -2,14 +2,11 @@ package ch.unibas.dmi.dbis.adam.evaluation.io
 
 import java.io._
 
-import adampro.grpc.shaded.com.google.protobuf.Descriptors
 import ch.unibas.dmi.dbis.adam.http.grpc.{DataMessage, QueryResultTupleMessage}
-import net.liftweb.json.JsonAST.JValue
+import net.liftweb.json.Extraction._
+import net.liftweb.json._
 
 import scala.collection.mutable.ListBuffer
-import scala.util.Random
-import net.liftweb.json.{JValue, _}
-import net.liftweb.json.Extraction._
 
 
 /**
@@ -19,59 +16,54 @@ import net.liftweb.json.Extraction._
   *
   * Created by silvanheller on 08.08.16.
   */
-object QueryResultIO extends App{
-  implicit val formats = net.liftweb.json.DefaultFormats
+object QueryResultIO extends App {
 
-  val qData = IndexedSeq.tabulate(20)(outer => Seq.tabulate(20)(el => QueryResultTupleMessage(Map("ap_distance" -> DataMessage().withIntData(el*outer), "information" -> DataMessage().withStringData("hello")))))
-  toFile(new File("resources/test.qrs"), qData)
+  val qData = IndexedSeq.tabulate(20)(outer => Seq.tabulate(20)(el => QueryResultTupleMessage(Map("ap_distance" -> DataMessage().withIntData(el * outer), "information" -> DataMessage().withStringData("hello")))))
+  writeToFile(new File("resources/test.qrs"), qData)
   val qRead = fromFile(new File("resources/test.qrs"))
 
-  if(qData!= qRead){
+  if (qData != qRead) {
     System.err.println("fail")
   } else System.out.println("heureka! ")
 
-  /**
-    * DataMessages are Stored with the toString-method and retrieved from Ascii for serialization purposes
-    *
-    * @param file
-    * @return
-    */
-  def fromFile(file: File) : IndexedSeq[Seq[QueryResultTupleMessage]] = {
+  /** DataMessages are Stored with the toString-method and retrieved from Ascii for serialization purposes */
+  def fromFile(file: File): IndexedSeq[Seq[QueryResultTupleMessage]] = {
+    implicit val formats = net.liftweb.json.DefaultFormats
     val lb = ListBuffer[Seq[QueryResultTupleMessage]]()
-    var br : BufferedReader = null
-    try{
+    var br: BufferedReader = null
+    try {
       br = new BufferedReader(new FileReader(file))
       var line = br.readLine()
-      while(line!=null){
+      while (line != null) {
         val res = parse(line).extract[List[Map[String, String]]]
-        if(line!=""){
-          lb+=res.map(m => QueryResultTupleMessage(m.map(el => (el._1, DataMessage.fromAscii(el._2)))))
+        if (line != "") {
+          lb += res.map(m => QueryResultTupleMessage(m.map(el => (el._1, DataMessage.fromAscii(el._2)))))
         }
         line = br.readLine()
       }
-    }finally br.close
+    } finally br.close()
     lb.toIndexedSeq
   }
 
-  def toFile(file: File, data: IndexedSeq[Seq[QueryResultTupleMessage]]) : Unit = {
-    var bw : BufferedWriter = null
-    if(!file.exists()){
-      if (file.getParentFile() != null) {
-        file.getParentFile().mkdirs()
-      }
-      file.createNewFile()
-      System.out.println("Created File"+file.getName)
+  def writeToFile(file: File, data: IndexedSeq[Seq[QueryResultTupleMessage]]): Unit = {
+    implicit val formats = net.liftweb.json.DefaultFormats
+    var bw: BufferedWriter = null
+    if (file.getParentFile != null) {
+      file.getParentFile.mkdirs()
     }
-    try{
+    file.delete()
+    file.createNewFile()
+    try {
       bw = new BufferedWriter(new FileWriter(file))
       var counter = 0
-      while(counter<data.size){
+      if (data.size > 2000) System.out.println("Writing data: " + file + " " + data.size)
+      while (counter < data.size) {
         val strmaps = data(counter).map(el => el.data.map(el => (el._1, el._2.toString()))).toList
-        bw.write(compactRender((decompose(strmaps))))
+        bw.write(compactRender(decompose(strmaps)))
         bw.newLine()
-        counter+=1
+        counter += 1
       }
       bw.flush()
-    }finally bw.close
+    } finally bw.close()
   }
 }
