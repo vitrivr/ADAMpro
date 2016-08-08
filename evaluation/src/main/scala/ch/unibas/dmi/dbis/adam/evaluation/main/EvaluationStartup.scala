bc569405274a26379fad3b7985653014c4238d99
Silvan Heller <silvan.heller@unibas.ch>
2016-08-08 15:01:11 +0200

    Added host-address debug output

--- evaluation/src/main/scala/ch/unibas/dmi/dbis/adam/evaluation/main/EvaluationStartup.scala
+++ evaluation/src/main/scala/ch/unibas/dmi/dbis/adam/evaluation/main/EvaluationStartup.scala
@@ -9,11 +9,12 @@ import ch.unibas.dmi.dbis.adam.evaluation.grpc.RPCClient
   * June 2016
   */
 object  EvaluationStartup {
-  val grpcHost = "10.34.58.136"
-  //val grpcHost="localhost"
+  //val grpcHost = "10.34.58.136"
+  val grpcHost="localhost"
   val grpcPort = 5890
 
   def main(args: Array[String]): Unit = {
+    System.out.println("Starting Evaluation on "+grpcHost+":"+grpcPort)
     val grpc: RPCClient = RPCClient(grpcHost, grpcPort)
   }
 }
