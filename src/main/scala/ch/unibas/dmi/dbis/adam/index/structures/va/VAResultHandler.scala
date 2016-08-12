11eb36c0c88677cc828abdc327a5fa781fc3b474
Ivan Giangreco <ivan.giangreco@unibas.ch>
2016-08-12 12:41:53 +0200

    improvements to VAResultHandler as suggested by Silvan

--- src/main/scala/ch/unibas/dmi/dbis/adam/index/structures/va/VAResultHandler.scala
+++ src/main/scala/ch/unibas/dmi/dbis/adam/index/structures/va/VAResultHandler.scala
@@ -1,6 +1,9 @@
 package ch.unibas.dmi.dbis.adam.index.structures.va
 
-import it.unimi.dsi.fastutil.floats.{FloatComparators, FloatHeapPriorityQueue}
+import java.util.Comparator
+
+import it.unimi.dsi.fastutil.floats.{FloatComparator, FloatComparators, FloatHeapPriorityQueue}
+import it.unimi.dsi.fastutil.objects.ObjectHeapPriorityQueue
 import org.apache.spark.sql.Row
 
 import scala.collection.mutable.ListBuffer
@@ -8,36 +11,44 @@ import scala.collection.mutable.ListBuffer
 /**
   * adamtwo
   *
-  * Ivan Giangreco
+  * Ivan Giangreco, improvements by Silvan Heller
   * August 2015
   */
 private[va] class VAResultHandler[A](k: Int) {
   @transient private var elementsLeft = k
-  @transient private val queue =  new FloatHeapPriorityQueue(2 * k, FloatComparators.OPPOSITE_COMPARATOR)
-  @transient protected var ls = ListBuffer[VAResultElement[A]]()
+
+  @transient private val upperBoundQueue = new FloatHeapPriorityQueue(2 * k, FloatComparators.OPPOSITE_COMPARATOR)
+  @transient private val lowerBoundResultElementQueue = new ObjectHeapPriorityQueue[VAResultElement[A]](2 * k, new VAResultElementLowerBoundComparator(FloatComparators.OPPOSITE_COMPARATOR))
+
+  private class VAResultElementLowerBoundComparator(comparator: FloatComparator) extends Comparator[VAResultElement[_]] with Serializable {
+    final def compare(a: VAResultElement[_], b: VAResultElement[_]): Int = comparator.compare(a.lower, b.lower)
+  }
+
 
   /**
     *
     * @param r
     */
-  def offer(r: Row, pk : String): Boolean = {
-    queue.synchronized {
-      if (elementsLeft > 0) { //we have not yet inserted k elements, no checks therefore
+  def offer(r: Row, pk: String): Boolean = {
+    upperBoundQueue.synchronized {
+      if (elementsLeft > 0) {
+        //we have not yet inserted k elements, no checks therefore
         val lower = r.getAs[Float]("lbound")
         val upper = r.getAs[Float]("ubound")
         val tid = r.getAs[A](pk)
         elementsLeft -= 1
         enqueueAndAddToCandidates(lower, upper, tid)
         return true
-      } else { //we have already k elements, therefore check if new element is better
-        val peek = queue.firstFloat()
+      } else {
+        //we have already k elements, therefore check if new element is better
+        //peek is the upper bound
+        val peek = upperBoundQueue.firstFloat()
         val lower = r.getAs[Float]("lbound")
         if (peek >= lower) {
-          //if peek is larger than lower, then dequeue worst element and insert
-          //new element
-          queue.dequeueFloat()
+          //if peek is larger than lower, then dequeue worst element and insert new element
+          upperBoundQueue.dequeueFloat()
           val upper = r.getAs[Float]("ubound")
-          val tid = r.getAs[A](pk : String)
+          val tid = r.getAs[A](pk: String)
           enqueueAndAddToCandidates(lower, upper, tid)
           return true
         } else {
@@ -53,7 +64,7 @@ private[va] class VAResultHandler[A](k: Int) {
     * @param upper
     * @param tid
     */
-  private def enqueueAndAddToCandidates(lower : Float, upper : Float, tid : A): Unit ={
+  private def enqueueAndAddToCandidates(lower: Float, upper: Float, tid: A): Unit = {
     enqueueAndAddToCandidates(VAResultElement(lower, upper, tid))
   }
 
@@ -62,8 +73,11 @@ private[va] class VAResultHandler[A](k: Int) {
     * @param res
     */
   private def enqueueAndAddToCandidates(res: VAResultElement[A]): Unit = {
-    queue.enqueue(res.upper)
-    ls += res
+    upperBoundQueue.enqueue(res.upper)
+    lowerBoundResultElementQueue.enqueue(res)
+    while (lowerBoundResultElementQueue.first().lower > upperBoundQueue.firstFloat()) {
+      lowerBoundResultElementQueue.dequeue()
+    }
   }
 
 
@@ -71,8 +85,17 @@ private[va] class VAResultHandler[A](k: Int) {
     *
     * @return
     */
-  def results = ls.toSeq
+  def results = {
+    val ls = ListBuffer[VAResultElement[A]]()
+
+    while (lowerBoundResultElementQueue.size() > 0) {
+      ls += lowerBoundResultElementQueue.dequeue()
+    }
+
+    ls.toSeq
+  }
 
 }
 
-case class VAResultElement[A](lower: Float, upper : Float, tid: A) {}
+case class VAResultElement[A](lower: Float, upper: Float, tid: A) {}
+
