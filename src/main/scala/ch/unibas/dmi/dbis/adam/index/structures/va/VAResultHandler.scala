6d93924007565ced7cd9d840283b66b71c221053
Ivan Giangreco <ivan.giangreco@unibas.ch>
2016-08-17 17:38:17 +0200

    adjustments to VA index: bitstring to cell only computed once (not for upper and lower bound), result handler now serializable, changed lbound ubound to ap_lbound ap_ubound

--- src/main/scala/ch/unibas/dmi/dbis/adam/index/structures/va/VAResultHandler.scala
+++ src/main/scala/ch/unibas/dmi/dbis/adam/index/structures/va/VAResultHandler.scala
@@ -14,11 +14,11 @@ import scala.collection.mutable.ListBuffer
   * Ivan Giangreco, improvements by Silvan Heller
   * August 2015
   */
-private[va] class VAResultHandler[A](k: Int) {
-  @transient private var elementsLeft = k
+private[va] class VAResultHandler[A](k: Int) extends Serializable {
+  private var elementsLeft = k
 
-  @transient private val upperBoundQueue = new FloatHeapPriorityQueue(2 * k, FloatComparators.OPPOSITE_COMPARATOR)
-  @transient private val lowerBoundResultElementQueue = new ObjectHeapPriorityQueue[VAResultElement[A]](2 * k, new VAResultElementLowerBoundComparator(FloatComparators.OPPOSITE_COMPARATOR))
+  private val upperBoundQueue = new FloatHeapPriorityQueue(2 * k, FloatComparators.OPPOSITE_COMPARATOR)
+  private val lowerBoundResultElementQueue = new ObjectHeapPriorityQueue[VAResultElement[A]](2 * k, new VAResultElementLowerBoundComparator(FloatComparators.OPPOSITE_COMPARATOR))
 
   private class VAResultElementLowerBoundComparator(comparator: FloatComparator) extends Comparator[VAResultElement[_]] with Serializable {
     final def compare(a: VAResultElement[_], b: VAResultElement[_]): Int = comparator.compare(a.lower, b.lower)
@@ -30,24 +30,24 @@ private[va] class VAResultHandler[A](k: Int) {
     * @param r
     */
   def offer(r: Row, pk: String): Boolean = {
-    upperBoundQueue.synchronized {
-      if (elementsLeft > 0) {
-        //we have not yet inserted k elements, no checks therefore
-        val lower = r.getAs[Float]("lbound")
-        val upper = r.getAs[Float]("ubound")
-        val tid = r.getAs[A](pk)
-        elementsLeft -= 1
-        enqueueAndAddToCandidates(lower, upper, tid)
-        return true
-      } else {
+    if (elementsLeft > 0) {
+      //we have not yet inserted k elements, no checks therefore
+      val lower = r.getAs[Float]("ap_lbound")
+      val upper = r.getAs[Float]("ap_ubound")
+      val tid = r.getAs[A](pk)
+      elementsLeft -= 1
+      enqueueAndAddToCandidates(lower, upper, tid)
+      return true
+    } else {
+      upperBoundQueue.synchronized {
         //we have already k elements, therefore check if new element is better
         //peek is the upper bound
         val peek = upperBoundQueue.firstFloat()
-        val lower = r.getAs[Float]("lbound")
+        val lower = r.getAs[Float]("ap_lbound")
         if (peek >= lower) {
           //if peek is larger than lower, then dequeue worst element and insert new element
           upperBoundQueue.dequeueFloat()
-          val upper = r.getAs[Float]("ubound")
+          val upper = r.getAs[Float]("ap_ubound")
           val tid = r.getAs[A](pk: String)
           enqueueAndAddToCandidates(lower, upper, tid)
           return true
@@ -97,5 +97,5 @@ private[va] class VAResultHandler[A](k: Int) {
 
 }
 
-case class VAResultElement[A](lower: Float, upper: Float, tid: A) {}
+case class VAResultElement[A](lower: Float, upper: Float, tid: A) extends Serializable {}
 
