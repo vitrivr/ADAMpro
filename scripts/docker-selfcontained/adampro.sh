bc097d349fca987a97e245fe61aa73ede2efb0ea
Ivan Giangreco <ivan.giangreco@unibas.ch>
2016-08-25 13:04:08 +0200

    small adjustments, comments

--- scripts/docker-selfcontained/adampro.sh
+++ scripts/docker-selfcontained/adampro.sh
@@ -11,7 +11,7 @@ cd $HADOOP_PREFIX/share/hadoop/common ; for cp in ${ACP//,/ }; do  echo == $cp; 
 
 # altering configurations
 sed s/HOSTNAME/$HOSTNAME/ /usr/local/hadoop/etc/hadoop/core-site.xml.template > /usr/local/hadoop/etc/hadoop/core-site.xml
-sed s/HOSTNAME/$HOSTNAME/ $SPARK_HOME/adampro.conf.template > $SPARK_HOME/adampro.conf
+sed s/HOSTNAME/$HOSTNAME/ $ADAM_HOME/adampro.conf.template > $ADAM_HOME/adampro.conf
 
 # setting spark defaults
 echo spark.yarn.jar hdfs:///localhost/spark-assembly-1.6.0-hadoop2.6.0.jar > $SPARK_HOME/conf/spark-defaults.conf
@@ -28,11 +28,11 @@ su --login - postgres --command "/usr/pgsql-9.4/bin/pg_ctl -w start -D $PGDATA"
 sleep 10 #sleep until HDFS is started
 
 hdfs dfsadmin -safemode leave
-$SPARK_HOME/bin/spark-submit --master "local[4]" --deploy-mode client --class ch.unibas.dmi.dbis.adam.main.Startup $SPARK_HOME/ADAMpro-assembly-0.1.0.jar &
+$SPARK_HOME/bin/spark-submit --master "local[4]" --deploy-mode client --class ch.unibas.dmi.dbis.adam.main.Startup $ADAM_HOME/ADAMpro-assembly-0.1.0.jar &
 
 sleep 10 #sleep until ADAMpro is started
 
-java -jar $SPARK_HOME/ADAMpro-web-assembly-0.1.0.jar
+java -jar $ADAM_HOME/ADAMpro-web-assembly-0.1.0.jar
 
 # start container always with option -d
 service sshd stop
