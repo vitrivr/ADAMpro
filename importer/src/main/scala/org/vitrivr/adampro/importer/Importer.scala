e53f01d1d025c619aaa53a505f19821eb6969e60
Ivan Giangreco <ivan.giangreco@unibas.ch>
2016-11-24 10:39:42 +0100

    logging import and possibly resuming

--- importer/src/main/scala/org/vitrivr/adampro/importer/Importer.scala
+++ importer/src/main/scala/org/vitrivr/adampro/importer/Importer.scala
@@ -1,5 +1,6 @@
 package org.vitrivr.adampro.importer
 
+import java.io.{BufferedWriter, File, FileWriter}
 import java.nio.file.{Files, Paths}
 
 import com.google.protobuf.CodedInputStream
@@ -8,7 +9,9 @@ import org.vitrivr.adampro.grpc.grpc.InsertMessage.TupleInsertMessage
 import org.vitrivr.adampro.grpc.grpc._
 import org.vitrivr.adampro.rpc.RPCClient
 
+import scala.collection.immutable.HashSet
 import scala.collection.mutable.ListBuffer
+import scala.io.Source
 
 
 /**
@@ -27,26 +30,117 @@ object Importer {
 
       val grpc = RPCClient(grpcHost, grpcPort)
 
-      if (args.length < 1) {
-        log.error("Usage: Importer path")
+      if (args.length < 2) {
+        log.error("Usage: Importer path logpath")
         sys.exit(1)
       }
 
-      importProtoFiles(args(0), grpc)
+      createEntities(grpc)
+      importProtoFiles(args(0), args(1), grpc)
     } catch {
       case e: Exception => log.error("error while importing", e)
     }
   }
 
+
+  /**
+    *
+    * @param grpc
+    */
+  def createEntities(grpc: RPCClient): Unit = {
+    //TODO: make this more generic, e.g., by importing entity creation messages
+
+    //metadata tables
+    grpc.entityCreate("cineast_multimediaobject",
+      Seq(
+        RPCAttributeDefinition("id", "string", true),
+        RPCAttributeDefinition("type", "int"),
+        RPCAttributeDefinition("name", "string"),
+        RPCAttributeDefinition("path", "string"),
+        RPCAttributeDefinition("width", "int"),
+        RPCAttributeDefinition("height", "int"),
+        RPCAttributeDefinition("framecount", "int"),
+        RPCAttributeDefinition("duration", "float")
+      )
+    )
+
+    grpc.entityCreate("cineast_segment",
+      Seq(
+        RPCAttributeDefinition("id", "string", true),
+        RPCAttributeDefinition("multimediaobject", "string"),
+        RPCAttributeDefinition("sequencenumber", "int"),
+        RPCAttributeDefinition("segmentstart", "int"),
+        RPCAttributeDefinition("segmentend", "int")
+      )
+    )
+
+    //feature tables
+    val features = Seq(
+      "features_AverageColor" -> Seq(("id", "string"), ("feature", "feature")),
+      "features_MedianColor" -> Seq(("id", "string"), ("feature", "feature")),
+      "features_AverageFuzzyHist" -> Seq(("id", "string"), ("feature", "feature")),
+      "features_AverageFuzzyHistNormalized" -> Seq(("id", "string"), ("feature", "feature")),
+      "features_MedianFuzzyHist" -> Seq(("id", "string"), ("feature", "feature")),
+      "features_AverageColorARP44" -> Seq(("id", "string"), ("feature", "feature")),
+      "features_AverageColorARP44Normalized" -> Seq(("id", "string"), ("feature", "feature")),
+      "features_SubDivAverageFuzzyColor" -> Seq(("id", "string"), ("feature", "feature")),
+      "features_SubDivMedianFuzzyColor" -> Seq(("id", "string"), ("feature", "feature")),
+      "features_AverageColorGrid8" -> Seq(("id", "string"), ("feature", "feature")),
+      "features_AverageColorGrid8Normalized" -> Seq(("id", "string"), ("feature", "feature")),
+      "features_AverageColorCLD" -> Seq(("id", "string"), ("feature", "feature")),
+      "features_AverageColorCLDNormalized" -> Seq(("id", "string"), ("feature", "feature")),
+      "features_CLD" -> Seq(("id", "string"), ("feature", "feature")),
+      "features_CLDNormalized" -> Seq(("id", "string"), ("feature", "feature")),
+      "features_MedianColorGrid8" -> Seq(("id", "string"), ("feature", "feature")),
+      "features_AverageColorRaster" -> Seq(("id", "string"), ("hist", "feature"), ("rater", "feature")),
+      "features_EdgeARP88" -> Seq(("id", "string"), ("feature", "feature")),
+      "features_EdgeGrid16" -> Seq(("id", "string"), ("feature", "feature")),
+      "features_EHD" -> Seq(("id", "string"), ("feature", "feature")),
+      "features_DominantEdgeGrid16" -> Seq(("id", "string"), ("feature", "feature")),
+      "features_DominantEdgeGrid8" -> Seq(("id", "string"), ("feature", "feature")),
+      "features_SubDivMotionHistogram3" -> Seq(("id", "string"), ("feature", "feature")),
+      "features_SubDivMotionHistogram5" -> Seq(("id", "string"), ("feature", "feature")),
+      "features_SubDivMotionHistogramBackground3" -> Seq(("id", "string"), ("feature", "feature")),
+      "features_SubDivMotionHistogramBackground5" -> Seq(("id", "string"), ("feature", "feature")),
+      "features_AverageColorGrid8Reduced11" -> Seq(("id", "string"), ("feature", "feature")),
+      "features_AverageColorGrid8Reduced15" -> Seq(("id", "string"), ("feature", "feature")),
+      "features_AverageColorRasterReduced11" -> Seq(("id", "string"), ("hist", "feature"), ("rater", "feature")),
+      "features_AverageColorRasterReduced15" -> Seq(("id", "string"), ("hist", "feature"), ("rater", "feature")),
+      "features_CLDReduced11" -> Seq(("id", "string"), ("feature", "feature")),
+      "features_CLDReduced15" -> Seq(("id", "string"), ("feature", "feature"))
+    )
+
+
+    features.foreach { case (entityname, attributes) =>
+      grpc.entityCreate(entityname,
+        attributes.map { case (name, datatype) => RPCAttributeDefinition(name, datatype, name == "id") }
+      )
+    }
+  }
+
+
   /**
     *
     * @param path
+    * @param proplogpath
     * @param grpc
     */
-  def importProtoFiles(path: String, grpc: RPCClient) = {
+  def importProtoFiles(path: String, proplogpath: String, grpc: RPCClient) = {
     if (!Paths.get(path).toFile.exists()) {
       throw new Exception("Path does not exist.")
     }
+
+    var logpath = proplogpath
+    var filter = HashSet[String]()
+
+    if (Paths.get(logpath).toFile.exists()) {
+      logpath = proplogpath + System.currentTimeMillis()
+
+      filter = HashSet().++(Source.fromFile(proplogpath).getLines)
+    }
+
+    val logfile = new File(logpath)
+    val pathLogger = new BufferedWriter(new FileWriter(logfile))
 
     val paths = files(path)
 
@@ -59,26 +153,30 @@ object Importer {
 
     paths.grouped(10).foreach(groupedPaths => {
       val batch = new ListBuffer[InsertMessage]()
-
+      val tmpPathLogs = new ListBuffer[String]()
       log.trace("starting new batch")
 
       groupedPaths.foreach { gpath =>
-        try {
-          val entity = gpath.getFileName.toString.replace(".bin", "")
-          val in = CodedInputStream.newInstance(Files.newInputStream(gpath))
+        if (!filter.contains(gpath.toAbsolutePath.toString)) {
+          try {
+            tmpPathLogs += gpath.toAbsolutePath.toString
 
-          while (!in.isAtEnd) {
-            val tuple = TupleInsertMessage.parseDelimitedFrom(in).get
+            val entity = gpath.getFileName.toString.replace(".bin", "")
+            val in = CodedInputStream.newInstance(Files.newInputStream(gpath))
 
-            val msg = InsertMessage(entity, Seq(tuple))
-            batch += msg
+            while (!in.isAtEnd) {
+              val tuple = TupleInsertMessage.parseDelimitedFrom(in).get
+
+              val msg = InsertMessage(entity, Seq(tuple))
+              batch += msg
+            }
+
+            this.synchronized {
+              remaining = remaining - 1
+            }
+          } catch {
+            case e: Exception => log.error("exception while reading files: " + gpath, e)
           }
-
-          this.synchronized {
-            remaining = remaining - 1
-          }
-        } catch {
-          case e: Exception => log.error("exception while reading files", e)
         }
       }
 
@@ -89,8 +187,16 @@ object Importer {
         log.error("exception while inserting files", res.failed.get)
       }
 
+      tmpPathLogs.foreach { tmpPathLog =>
+        pathLogger.write(tmpPathLog)
+        pathLogger.write("\n")
+      }
+      pathLogger.flush()
+
       log.debug("status: " + remaining + "/" + length)
     })
+
+    pathLogger.close()
   }
 
   /**
