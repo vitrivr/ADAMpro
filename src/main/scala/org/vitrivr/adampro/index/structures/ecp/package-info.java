1cfc1d74afb2424a044f55676422395ee57c76ff
Ivan Giangreco <ivan.giangreco@unibas.ch>
2017-01-30 17:00:17 +0100

    correcting the citations for indexes

--- src/main/scala/org/vitrivr/adampro/index/structures/ecp/package-info.java
+++ src/main/scala/org/vitrivr/adampro/index/structures/ecp/package-info.java
@@ -1,7 +1,7 @@
 /**
  * Extended Cluster Pruning
  * <p>
- * see F. Chierichetti, A. Panconesi, P. Raghavan, M. Sozio, A. Tiberi, E. Upfal (2007). Finding near neighbors through cluster pruning. Proceedings of the ACM SIGMOD-SIGACT-SIGART symposium on Principles of database systems..
+ * see F. Chierichetti, A. Panconesi, P. Raghavan, M. Sozio, A. Tiberi, E. Upfal (2007). Finding near neighbors through cluster pruning. Proceedings of the ACM SIGMOD-SIGACT-SIGART symposium on Principles of database systems.
  */
 package org.vitrivr.adampro.index.structures.ecp;
 
--- src/main/scala/org/vitrivr/adampro/index/structures/pq/package-info.java
+++ src/main/scala/org/vitrivr/adampro/index/structures/pq/package-info.java
@@ -1,7 +1,7 @@
 /**
  * Product Quantization
  * <p>
- * see H. Jégou, M. Douze, C. Schmid (2008): Product quantization for nearest neighbor search. IEEE Transactions on Pattern Analysis & Machine Intelligence.
+ * see H. Jegou, M. Douze, C. Schmid (2008): Product quantization for nearest neighbor search. IEEE Transactions on Pattern Analysis & Machine Intelligence.
  * <p>
  * The implementation of product quantization is based on the Matlab code provided by the authors on http://people.rennes.inria.fr/Herve.Jegou/projects/ann.html
  */
