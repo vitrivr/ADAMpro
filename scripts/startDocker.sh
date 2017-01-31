8861243f38e8d06be8f3a54db2f43d735507b49e
Ivan Giangreco <ivan.giangreco@unibas.ch>
2017-01-31 14:14:13 +0100

    renaming network and hostname in Dockerfile

--- scripts/startDocker.sh
+++ scripts/startDocker.sh
@@ -6,9 +6,9 @@
 
 ADAMPRODIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ".." && pwd )"
 
-docker network create --driver bridge adampronw
+docker network create --driver bridge vitrvrnw
 docker build -t adampro $ADAMPRODIR/scripts/docker
-docker run --net=adampronw -d -h adamprovbs --name adamprovbs --net-alias adamprovbs -p 5890:5890 -p 9099:9099 -p 5432:5432 -p 9000:9000 -p 4040:4040 -d adampro
+docker run --net=vitrvrnw -d -h adampro --name adampro --net-alias adampro -p 5890:5890 -p 9099:9099 -p 5432:5432 -p 9000:9000 -p 4040:4040 -d adampro
 
 echo "Self-contained ADAMpro container has been built and started; open http://localhost:4040"
 echo "Run ´docker save -o adampro.tar adampro´ to export."
