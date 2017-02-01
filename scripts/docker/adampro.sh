31a3612c696043ab0300c2947fdb7868eb7c5090
Ivan Giangreco <ivan.giangreco@unibas.ch>
2017-02-01 17:11:43 +0100

    added possbility to use remote debugger on port 5005

--- scripts/docker/adampro.sh
+++ scripts/docker/adampro.sh
@@ -13,8 +13,9 @@ su --login - postgres --command "$POSTGRES_HOME/bin/pg_ctl -w start -D $PGDATA"
 solr start -noprompt &
 
 # run ADAMpro
-$SPARK_HOME/bin/spark-submit --master "local[4]" --driver-memory 5g --executor-memory 5g --deploy-mode client --class org.vitrivr.adampro.main.Startup $ADAM_HOME/ADAMpro-assembly-0.1.0.jar &
-java -jar $ADAM_HOME/ADAMpro-web-assembly-0.1.0.jar &
+export SPARK_SUBMIT_OPTS=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005
+$SPARK_HOME/bin/spark-submit --master "local[4]" --driver-memory 5g --executor-memory 5g --deploy-mode client --driver-java-options "-Dlog4j.configuration=file:$ADAM_HOME/log4j2.xml" --conf "spark.executor.extraJavaOptions=-Dlog4j.configuration=file:$ADAM_HOME/log4j2.xml" --class org.vitrivr.adampro.main.Startup $ADAM_HOME/ADAMpro-assembly-0.1.0.jar
+java -jar $ADAM_HOME/ADAMpro-web-assembly-0.1.0.jar
 
 while true; do sleep 1000; done
 
--- scripts/setupDocker.sh
+++ scripts/setupDocker.sh
@@ -8,7 +8,7 @@ ADAMPRODIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ".." && pwd )"
 
 docker network create --driver bridge vitrvrnw
 docker build -t adampro $ADAMPRODIR/scripts/docker
-docker run --net=vitrvrnw -d -h adampro --name adampro --net-alias adampro -p 5890:5890 -p 9099:9099 -p 5432:5432 -p 9000:9000 -p 4040:4040 -d adampro
+docker run --net=vitrvrnw -d -h adampro --name adampro --net-alias adampro -p 5005:5005 -p 5890:5890 -p 9099:9099 -p 5432:5432 -p 9000:9000 -p 4040:4040 -d adampro
 
 echo "Self-contained ADAMpro container has been built and started; open http://localhost:4040"
 echo "Run ´docker save -o adampro.tar adampro´ to export."
