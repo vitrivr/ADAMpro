6f7479319ed9b78dc40be3a437e5ea2cf0745d5a
Ivan Giangreco <ivan.giangreco@unibas.ch>
2017-02-13 18:28:39 +0100

    starting docker container with notebook port open

--- scripts/setupDocker.sh
+++ scripts/setupDocker.sh
@@ -11,7 +11,7 @@ docker network create --driver bridge vitrvrnw
 docker build -t adampro $ADAMPRODIR/scripts/docker
 
 # run container
-docker run --net=vitrvrnw -d -h adampro --name adampro --net-alias adampro -p 5005:5005 -p 5890:5890 -p 9099:9099 -p 5432:5432 -p 9000:9000 -p 4040:4040 -d adampro
+docker run --net=vitrvrnw -d -h adampro --name adampro --net-alias adampro -p 5005:5005 -p 5890:5890 -p 9099:9099 -p 5432:5432 -p 9000:9000 -p 4040:4040 -p 10088:10088 -d adampro
 
 # output
 echo "Self-contained ADAMpro container has been built and started; open http://localhost:4040"
