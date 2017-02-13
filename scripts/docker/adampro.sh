4546464bad922b005e74fae205ce8fdaf20ee6b1
Ivan Giangreco <ivan.giangreco@unibas.ch>
2017-02-13 17:28:00 +0100

    adjusted Docker file: added notebook, added environment variables to stop postgresql, solr or web UI from running

--- scripts/docker/adampro.sh
+++ scripts/docker/adampro.sh
@@ -4,15 +4,24 @@
 sed s/HOSTNAME/$HOSTNAME/ $ADAM_HOME/code/scripts/docker/adampro.conf.template > $ADAM_HOME/adampro.conf
 
 # storage engines
-service postgresql stop
-su --login - postgres --command "$POSTGRES_HOME/bin/pg_ctl -w start -D $PGDATA"
+if [[( -z "$ADAMPRO_START_POSTGRES" ) || ( "$ADAMPRO_START_POSTGRES" == "true")]]; then
+    service postgresql stop
+    su --login - postgres --command "$POSTGRES_HOME/bin/pg_ctl -w start -D $PGDATA"
+fi
 
-solr start -noprompt &
+# start solr
+if [[ (-z "$ADAMPRO_START_SOLR" ) || ( "$ADAMPRO_START_SOLR" == "true")]]; then
+    solr start -noprompt &
+fi
 
 # run ADAMpro
 export SPARK_SUBMIT_OPTS=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005
-$SPARK_HOME/bin/spark-submit --master "local[4]" --driver-memory 2g --executor-memory 2g --deploy-mode client --driver-java-options "-Dlog4j.configuration=file:$ADAM_HOME/log4j.xml" --conf "spark.executor.extraJavaOptions=-Dlog4j.configuration=file:$ADAM_HOME/log4j.xml" --driver-java-options "-XX:+UnlockCommercialFeatures -XX:+FlightRecorder" --conf "spark.executor.extraJavaOptions=-XX:+UnlockCommercialFeatures -XX:+FlightRecorder" --class org.vitrivr.adampro.main.Startup $ADAM_HOME/ADAMpro-assembly-0.1.0.jar &
-java -jar $ADAM_HOME/ADAMpro-web-assembly-0.1.0.jar &
+$SPARK_HOME/bin/spark-submit --master "$ADAMPRO_MASTER" --driver-memory 2g --executor-memory 2g --deploy-mode client --driver-java-options "-Dlog4j.configuration=file:$ADAM_HOME/log4j.xml" --conf "spark.executor.extraJavaOptions=-Dlog4j.configuration=file:$ADAM_HOME/log4j.xml" --driver-java-options "-XX:+UnlockCommercialFeatures -XX:+FlightRecorder" --conf "spark.executor.extraJavaOptions=-XX:+UnlockCommercialFeatures -XX:+FlightRecorder" --class org.vitrivr.adampro.main.Startup $ADAM_HOME/ADAMpro-assembly-0.1.0.jar &
+
+# start web UI
+if [[ ( -z "$ADAMPRO_START_WEBUI" ) || ( "$ADAMPRO_START_WEBUI" == "true")]]; then
+    java -jar $ADAM_HOME/ADAMpro-web-assembly-0.1.0.jar &
+fi
 
 while true; do sleep 1000; done
 
