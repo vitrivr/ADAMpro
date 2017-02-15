137ca52d63015de7e71f0deb25ed4e3cd164f18e
Ivan Giangreco <ivan.giangreco@unibas.ch>
2017-02-15 17:23:00 +0100

    adjusted Docker for HDFS to run node formatting only the first time

--- scripts/docker-hdfs/bootstrap.sh
+++ scripts/docker-hdfs/bootstrap.sh
@@ -19,6 +19,17 @@ cd $HADOOP_HOME/share/hadoop/common ; for cp in ${ACP//,/ }; do  echo == $cp; cu
 if [[ $1 = "-masternode" || $2 = "-masternode" ]]; then
    echo "starting master..."
 
+   if [ -f $HADOOP_FIRST_STARTUP ]; then
+      echo "setting up hadoop for first time run..."
+      $HADOOP_HOME/bin/hadoop namenode -format
+      ${HADOOP_HOME}/sbin/start-dfs.sh
+      ${HADOOP_HOME}/bin/hdfs dfs -mkdir -p /adampro/data/data
+      ${HADOOP_HOME}/bin/hdfs dfs -mkdir -p /adampro/data/index
+      ${HADOOP_HOME}/sbin/stop-dfs.sh
+
+      rm $HADOOP_FIRST_STARTUP
+  fi
+
   service dnsmasq start
   $HADOOP_PREFIX/sbin/start-yarn.sh
   $HADOOP_PREFIX/sbin/start-dfs.sh
@@ -39,9 +50,6 @@ fi
 if [[ $1 = "-workernode" || $2 = "-workernode" ]]; then
   echo "starting worker..."
 
-  rm -r /hdfs/namenode/*
-  rm -r /hdfs/datanode/*
-
   HN=`hostname`
   IP=`ifconfig eth0 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}'`
   ssh $MASTER_HOSTNAME "grep -q -F $HN /etc/hosts || ( echo $IP $HN >> /etc/hosts && service dnsmasq restart )"
@@ -56,8 +64,6 @@ if [[ $1 = "-workernode" || $2 = "-workernode" ]]; then
 fi
 
 if [[ $1 = "-d" || $2 = "-d" ]]; then
-  echo "starting -d..."
-
   while true; do ssh $MASTER_HOSTNAME cat /etc/hosts | grep -v localhost | grep -v :: | grep -v $MASTER_HOSTNAME | grep -v `hostname` | while read line ; do grep "$line" /etc/hosts > /dev/null 2>&1 || (echo "$line" >> /etc/hosts); done ; sleep 60 ; done
 fi
 
