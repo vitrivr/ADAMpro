c36b92ae4f4fac2b04a8c965b3db7ab4f5e169f6
Ivan Giangreco <ivan.giangreco@unibas.ch>
2017-03-20 15:12:58 +0100

    w.r.t. [SPARK-19629], added option openCostInBytes

--- src/main/scala/org/vitrivr/adampro/main/SparkStartup.scala
+++ src/main/scala/org/vitrivr/adampro/main/SparkStartup.scala
@@ -28,7 +28,9 @@ object SparkStartup extends Logging {
         .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
         .set("spark.kryoserializer.buffer.max", "2047m")
         .set("spark.kryoserializer.buffer", "2047")
-        .set("spark.sql.autoBroadcastJoinThreshold", "52428800")
+        .set("spark.sql.autoBroadcastJoinThreshold", (50 * 1024 * 1024).toString)
+        .set("spark.sql.files.openCostInBytes", (256 * 1024 * 1024).toString) // see [SPARK-19629]
+
 
       if (config.master.isDefined) {
         conf.setMaster(config.master.get)
