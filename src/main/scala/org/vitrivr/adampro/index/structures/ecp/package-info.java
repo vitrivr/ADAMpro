510453774ccd29cba4db24af109faed98657d983
Ivan Giangreco <ivan.giangreco@unibas.ch>
2017-05-17 14:29:50 +0200

    adjusted comments of ecp and mi

--- src/main/scala/org/vitrivr/adampro/index/structures/ecp/package-info.java
+++ src/main/scala/org/vitrivr/adampro/index/structures/ecp/package-info.java
@@ -1,7 +1,8 @@
 /**
- * Extended Cluster Pruning
+ * (Extended) Cluster Pruning
  * <p>
  * see F. Chierichetti, A. Panconesi, P. Raghavan, M. Sozio, A. Tiberi, E. Upfal (2007). Finding near neighbors through cluster pruning. Proceedings of the ACM SIGMOD-SIGACT-SIGART symposium on Principles of database systems.
+ * see Gudmundsson, G. Þ., Jónsson, B. Þ., & Amsaleg, L. (2010). A Large-Scale Performance Study of Cluster-based High-Dimensional Indexing. In Proceedings of the International Workshop on Very-Large-Scale Multimedia Corpus, Mining and Retrieval (VLS-MCMR).
  */
 package org.vitrivr.adampro.index.structures.ecp;
 
--- src/main/scala/org/vitrivr/adampro/index/structures/mi/package-info.java
+++ src/main/scala/org/vitrivr/adampro/index/structures/mi/package-info.java
@@ -1,7 +1,8 @@
 /**
  * MI-File
  * <p>
- * see G. Amato, C. Gennaro and P. Savino (2014): MI-File: Using Inverted Files for Scalable Approximate Similarity Search.
+ * see Chavez Gonzalez, E., Figueroa, K., & Navarro, G. (2008). Effective Proximity Retrieval by Ordering Permutations. IEEE Transactions on Pattern Analysis and Machine Intelligence.
+ * see G. Amato, C. Gennaro and P. Savino (2014). MI-File: Using Inverted Files for Scalable Approximate Similarity Search.
  * <p>
  * The implementation of MI-File is based on the code provided by the authors on https://github.com/giamato/mi-file/
  */
