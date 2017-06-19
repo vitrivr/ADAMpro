33efe49dcfadd6fe5a245dcb433466cddc1a6e95
Ivan Giangreco <ivan.giangreco@unibas.ch>
2017-06-19 16:30:22 +0200

    improvements to result handler for higher speed

--- src/main/scala/org/vitrivr/adampro/index/structures/va/VAResultHandler.scala
+++ src/main/scala/org/vitrivr/adampro/index/structures/va/VAResultHandler.scala
@@ -3,10 +3,8 @@ package org.vitrivr.adampro.index.structures.va
 import java.util.Comparator
 
 import it.unimi.dsi.fastutil.doubles.{DoubleComparator, DoubleComparators, DoubleHeapPriorityQueue}
-import it.unimi.dsi.fastutil.objects.ObjectHeapPriorityQueue
 import org.apache.spark.sql.Row
 import org.vitrivr.adampro.datatypes.TupleID._
-import org.vitrivr.adampro.query.distance.Distance
 import org.vitrivr.adampro.query.distance.Distance.Distance
 import org.vitrivr.adampro.utils.Logging
 
@@ -22,7 +20,7 @@ private[va] class VAResultHandler(k: Int) extends Logging with Serializable {
   private var elementsLeft = k
 
   private val upperBoundQueue = new DoubleHeapPriorityQueue(2 * k, DoubleComparators.OPPOSITE_COMPARATOR)
-  private val lowerBoundResultElementQueue = new ObjectHeapPriorityQueue[VAResultElement](2 * k, new VAResultElementLowerBoundComparator(DoubleComparators.OPPOSITE_COMPARATOR))
+  private val lowerBoundResultElementQueue = new ListBuffer[VAResultElement]()
 
   private class VAResultElementLowerBoundComparator(comparator: DoubleComparator) extends Comparator[VAResultElement] with Serializable {
     final def compare(a: VAResultElement, b: VAResultElement): Int = comparator.compare(a.ap_lower, b.ap_lower)
@@ -43,21 +41,20 @@ private[va] class VAResultHandler(k: Int) extends Logging with Serializable {
       enqueueAndAddToCandidates(tid, lower, upper)
       return true
     } else {
-      this.synchronized {
-        //we have already k elements, therefore check if new element is better
-        //peek is the upper bound
-        val peek = upperBoundQueue.first()
-        val lower = r.getAs[Distance]("ap_lbound")
-        if (peek >= lower) {
-          //if peek is larger than lower, then dequeue worst element and insert new element
-          upperBoundQueue.dequeue()
-          val upper = r.getAs[Distance]("ap_ubound")
-          val tid = r.getAs[TupleID](pk: String)
-          enqueueAndAddToCandidates(tid, lower, upper)
-          return true
-        } else {
-          return false
-        }
+      val lower = r.getAs[Distance]("ap_lbound")
+
+      //we have already k elements, therefore check if new element is better
+      //peek is the upper bound
+      val peek = upperBoundQueue.first()
+      if (peek >= lower) {
+        //if peek is larger than lower, then dequeue worst element and insert new element
+        upperBoundQueue.dequeue()
+        val upper = r.getAs[Distance]("ap_ubound")
+        val tid = r.getAs[TupleID](pk: String)
+        enqueueAndAddToCandidates(tid, lower, upper)
+        return true
+      } else {
+        return false
       }
     }
   }
@@ -75,23 +72,21 @@ private[va] class VAResultHandler(k: Int) extends Logging with Serializable {
       enqueueAndAddToCandidates(res.ap_id, res.ap_lower, res.ap_upper)
       return true
     } else {
-      this.synchronized {
-        //we have already k elements, therefore check if new element is better
-        //peek is the upper bound
-        val peek =  upperBoundQueue.first()
-        if (peek >= res.ap_lower) {
-          //if peek is larger than lower, then dequeue worst element and insert new element
-          upperBoundQueue.dequeue()
-          enqueueAndAddToCandidates(res.ap_id, res.ap_lower,  res.ap_upper)
-          return true
-        } else {
-          return false
-        }
+      //we have already k elements, therefore check if new element is better
+      //peek is the upper bound
+      val peek = upperBoundQueue.first()
+      if (peek >= res.ap_lower) {
+        //if peek is larger than lower, then dequeue worst element and insert new element
+        upperBoundQueue.dequeue()
+        enqueueAndAddToCandidates(res.ap_id, res.ap_lower, res.ap_upper)
+        return true
+      } else {
+        return false
       }
     }
   }
 
-    /**
+  /**
     *
     * @param tid
     * @param lower
@@ -106,13 +101,8 @@ private[va] class VAResultHandler(k: Int) extends Logging with Serializable {
     * @param res
     */
   private def enqueueAndAddToCandidates(res: VAResultElement): Unit = {
-    this.synchronized {
-      upperBoundQueue.enqueue(res.ap_upper)
-      lowerBoundResultElementQueue.enqueue(res)
-      while (!lowerBoundResultElementQueue.isEmpty && lowerBoundResultElementQueue.first().ap_lower > upperBoundQueue.first()) {
-        lowerBoundResultElementQueue.dequeue()
-      }
-    }
+    upperBoundQueue.enqueue(res.ap_upper)
+    lowerBoundResultElementQueue += res
   }
 
 
@@ -121,15 +111,8 @@ private[va] class VAResultHandler(k: Int) extends Logging with Serializable {
     * @return
     */
   def results = {
-    val ls = ListBuffer[VAResultElement]()
-
-    this.synchronized {
-      while (lowerBoundResultElementQueue.size() > 0) {
-        ls += lowerBoundResultElementQueue.dequeue()
-      }
-    }
-
-    ls
+    val maxUpperBound = upperBoundQueue.first()
+    lowerBoundResultElementQueue.filterNot(_.ap_lower > maxUpperBound)
   }
 
 }
