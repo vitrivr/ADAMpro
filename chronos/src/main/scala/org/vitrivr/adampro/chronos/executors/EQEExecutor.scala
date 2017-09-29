b87c8dc91c6afa96fe90ad59307958ea08497ba7
Ivan Giangreco <ivan.giangreco@unibas.ch>
2017-09-29 10:06:45 +0200

    EQE executor adjusted for evaluation

--- chronos/src/main/scala/org/vitrivr/adampro/chronos/executors/EQEExecutor.scala
+++ chronos/src/main/scala/org/vitrivr/adampro/chronos/executors/EQEExecutor.scala
@@ -33,13 +33,13 @@ class EQEExecutor(job: EvaluationJob, setStatus: (Double) => (Boolean), inputDir
 
     logger.info("started adjusting query weights")
 
-    val nqueries = if(job.execution_nqueries.isEmpty){
+    val nqueries = if (job.execution_nqueries.isEmpty) {
       None
     } else {
       Some(job.execution_nqueries.toInt)
     }
 
-    val nruns = if(job.execution_nruns.isEmpty){
+    val nruns = if (job.execution_nruns.isEmpty) {
       None
     } else {
       Some(job.execution_nruns.toInt)
@@ -56,32 +56,46 @@ class EQEExecutor(job: EvaluationJob, setStatus: (Double) => (Boolean), inputDir
 
     val queryProgressAddition = (1 - getStatus) / queries.size.toFloat
 
+    val executions = job.execution_subexecution.map(x => (x._1.toLowerCase, x._2))
+
     //query execution
     queries.zipWithIndex.foreach { case (qo, idx) =>
       if (running) {
         val runid = "run_" + idx.toString
         logger.info("executing query for " + entityname.get + " (runid: " + runid + ")")
 
-        val scoring = client.getScoredQueryExecutionPaths(qo, job.execution_subtype).get.map(x => (x._1, x._2, x._3))
+        val scoredPaths = client.getScoredQueryExecutionPaths(qo, job.execution_subtype).get
+        val scoring = scoredPaths.map(x => (x._1, x._2, x._3))
           .groupBy(_._2).mapValues(_.sortBy(_._3).reverse.head).toMap
         //index name, type, score; take only maximum
 
         val result = executeQuery(qo)
 
         //execute all other scans for comparison
-        job.execution_subexecution.map { case (indextype, withsequential) => (indextype, scoring.get(indextype)) }
-          .filter(_._2.isDefined)
+        executions.map { case (indextype, withsequential) => (indextype, scoring.get(indextype.toLowerCase)) }.filter(_._2.isDefined)
           .filterNot(x => x._1 == "sequential")
           .foreach { case (indextype, scoring) =>
             val tmpQo = new RPCIndexScanQueryObject(qo.id, qo.options ++ Seq("indexname" -> scoring.get._1))
             val tmpResult = executeQuery(tmpQo) ++ Seq("scanscore" -> scoring.get._3.toString)
-            results += (runid + "-loosers-" + indextype -> tmpResult)
+
+            if (job.measurement_firstrun && idx < NFIRST_RUN_QUERIES) {
+              //ignore first run
+            } else {
+              results += (runid + "-loosers-" + indextype -> tmpResult)
+            }
+
+
           }
 
-        if(job.execution_subexecution.map(_._1).contains("sequential")){
+        if (executions.map(_._1).contains("sequential")) {
           val tmpQo = new RPCSequentialScanQueryObject(qo.id, qo.options)
           val tmpResult = executeQuery(tmpQo) ++ Seq("scanscore" -> scoring.get("sequential").map(_._3).getOrElse(-1).toString)
-          results += (runid + "-loosers-" + "sequential" -> tmpResult)
+
+          if (job.measurement_firstrun && idx < NFIRST_RUN_QUERIES) {
+            //ignore first run
+          } else {
+            results += (runid + "-loosers-" + "sequential" -> tmpResult)
+          }
         }
 
         logger.info("executed query for " + entityname.get + " (runid: " + runid + ")")
@@ -120,7 +134,7 @@ class EQEExecutor(job: EvaluationJob, setStatus: (Double) => (Boolean), inputDir
     * @param sparseQuery
     * @return
     */
-  override protected def getQuery(entityname: String, k: Int, sparseQuery: Boolean, options : Seq[(String, String)] = Seq()): RPCGenericQueryObject = {
+  override protected def getQuery(entityname: String, k: Int, sparseQuery: Boolean, options: Seq[(String, String)] = Seq()): RPCGenericQueryObject = {
     val id = Helpers.generateString(10)
 
     val lb = new ListBuffer[(String, String)]()
@@ -159,5 +173,39 @@ class EQEExecutor(job: EvaluationJob, setStatus: (Double) => (Boolean), inputDir
     lb.append(("nofallback" -> "true"))
 
     RPCEmpiricalScanQueryObject(id, (options ++ lb).toMap)
+  }
+
+  /**
+    *
+    * @param results
+    * @return
+    */
+  override protected def prepareResults(results: ListBuffer[(String, Map[String, String])]) = {
+    val props = super.prepareResults(results)
+
+    val executions = job.execution_subexecution.map(x => (x._1.toLowerCase, x._2))
+
+    props.setProperty("summary_desc", executions.map(_._1).mkString(","))
+
+    executions.foreach { case(execution, withSequential) =>
+      val filteredForExecution = results.filter(_._1.contains(execution)).map(_._2)
+      val times = filteredForExecution.map(_.get("totaltime").getOrElse("-1"))
+      val scanscores = filteredForExecution.map(_.get("scanscore").getOrElse("-1"))
+
+      props.setProperty("summary_totaltimes_" + execution, times.mkString(","))
+      props.setProperty("summary_scanscores_" + execution, scanscores.mkString(","))
+      props.setProperty("summary_expectedtimes_" + execution, scanscores.map(score => 1.toDouble / score.toDouble).mkString(","))
+
+      val metrics = filteredForExecution.flatMap(_.keySet.filter(_.contains("resultquality-")).map(_.replace("resultquality-", ""))).toSet
+
+      metrics.foreach{ metric =>
+        val qualitymeasures = filteredForExecution.map(_.get("resultquality-" + metric).getOrElse("-1"))
+        props.setProperty("summary_resultquality_" + metric + "_" + execution, qualitymeasures.mkString(","))
+      }
+
+      props.setProperty("summary_resultquality_measures_" + execution, metrics.map(metric => "summary_resultquality_" + metric + "_" + execution).mkString(","))
+    }
+
+    props
   }
 }
