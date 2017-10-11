d0e3ada7b9253bac9aeebf9e8ba3a87ac8e5b32a
Ivan Giangreco <ivan.giangreco@unibas.ch>
2017-10-11 10:26:23 +0200

    allowing optimiser to be updated by storing training data

--- src/main/scala/org/vitrivr/adampro/utils/ml/LinearRegression.scala
+++ src/main/scala/org/vitrivr/adampro/utils/ml/LinearRegression.scala
@@ -1,11 +1,15 @@
 package org.vitrivr.adampro.utils.ml
 
 
+import java.io.File
+
 import breeze.linalg.DenseVector
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.regression.{LabeledPoint, LinearRegressionModel, LinearRegressionWithSGD}
 import org.vitrivr.adampro.process.SharedComponentContext
 import org.vitrivr.adampro.utils.Logging
+
+
 
 /**
   * ADAMpro
@@ -14,7 +18,7 @@ import org.vitrivr.adampro.utils.Logging
   * August 2017
   */
 class LinearRegression(val path : String)(@transient implicit val ac: SharedComponentContext) extends Logging with Serializable {
-  val model : LinearRegressionModel = LinearRegressionModel.load(ac.sc, path)
+  val model : LinearRegressionModel = LinearRegressionModel.load(ac.sc, path  + LinearRegression.MODEL_SUFFIX)
 
 
   /**
@@ -26,12 +30,30 @@ class LinearRegression(val path : String)(@transient implicit val ac: SharedComp
 }
 
 object LinearRegression {
+  val MODEL_SUFFIX = ".model"
+  val DATA_SUFFIX = ".data"
+
   def train(data: Seq[TrainingSample], path : String)(implicit ac : SharedComponentContext): Unit ={
     val labelledData = data.map(x => LabeledPoint(x.time, Vectors.dense(x.f.toArray)))
+    val rdd = ac.sc.parallelize(labelledData)
+
+    rdd.saveAsObjectFile(path + "/data/data_" + (System.currentTimeMillis() / 1000L).toString + DATA_SUFFIX)
+
+    val modelFile = new File(path  + MODEL_SUFFIX)
+
+    if(modelFile.exists()){
+      modelFile.delete()
+    }
 
     val lr = new LinearRegressionWithSGD()
-    val model = lr.run(ac.sc.parallelize(labelledData))
 
-    model.save(ac.sc, path)
+    val trainingData = new File(path + "/data/").listFiles.filter(_.isFile).filter(_.getName.endsWith(DATA_SUFFIX)).map{ dataFile =>
+      ac.sc.objectFile[LabeledPoint](dataFile.getAbsolutePath)
+    }.reduce(_ union _)
+
+
+    val model = lr.run(trainingData)
+
+    model.save(ac.sc, path + MODEL_SUFFIX)
   }
 }
