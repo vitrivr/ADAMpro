efc0d4ef2bc2447345fef0935f878cb80401c925
Ivan Giangreco <ivan.giangreco@unibas.ch>
2017-11-01 19:09:03 +0100

    update version in setup script for local test

--- scripts/setupLocalUnitTests.sh
+++ scripts/setupLocalUnitTests.sh
@@ -13,13 +13,13 @@ docker network create --driver bridge vitrvrnw
 docker run --net=vitrvrnw -p 5432:5432 -e POSTGRES_PASSWORD=adampro -e POSTGRES_DB=adampro -e POSTGRES_USER=adampro  -h postgresql --name postgresql --net-alias postgresql -d postgres:9.4
 
 # creates a solr container
-docker run --net=vitrvrnw -p 8983:8983 -h solr --name solr --net-alias solr -d solr:6.1
+docker run --net=vitrvrnw -p 8983:8983 -h solr --name solr --net-alias solr -d solr:7.1
 
 # create a postgis container
 docker run --net=vitrvrnw -p 5433:5432 -e POSTGRES_PASSWORD=adampro -e POSTGRES_DB=adampro -e POSTGRES_USER=adampro  -h postgis --name postgis --net-alias postgis -d mdillon/postgis
 
 # create a cassandra container
-docker run --net=vitrvrnw -p 9042:9042 -h cassandra --name cassandra --net-alias cassandra -d cassandra:3.7
+docker run --net=vitrvrnw -p 9042:9042 -h cassandra --name cassandra --net-alias cassandra -d cassandra:3.11
 
 # output
 echo "Containers for running ADAMpro have been set up"
