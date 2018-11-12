48ffd2b7ae876b1cda2e515b64de20fab05884b3
Ivan Giangreco <ivan.giangreco@unibas.ch>
2018-11-12 15:41:44 +0100

    cassandra startup delayed

--- scripts/docker-selfcontained/bootstrap.sh
+++ scripts/docker-selfcontained/bootstrap.sh
@@ -11,6 +11,12 @@ if [[ (-z "$ADAMPRO_START_SOLR" ) || ( "$ADAMPRO_START_SOLR" == "true")]]; then
     solr start -noprompt &
 fi
 
+# start cassandra
+if [[ (-z "$ADAMPRO_START_CASSANDRA" ) || ( "$ADAMPRO_START_CASSANDRA" == "true")]]; then
+    /usr/local/bin/docker-entrypoint.sh &
+    wait-for-it.sh
+fi
+
 # run ADAMpro
 export SPARK_SUBMIT_OPTS=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005
 $SPARK_HOME/bin/spark-submit --master "$ADAMPRO_MASTER" --driver-memory "$ADAMPRO_MEMORY" --executor-memory "$ADAMPRO_MEMORY" --deploy-mode client --driver-java-options "-Dlog4j.configuration=file:$ADAMPRO_HOME/log4j.properties" --conf "spark.executor.extraJavaOptions=-Dlog4j.configuration=file:$ADAMPRO_HOME/log4j.properties" --conf "spark.sql.broadcastTimeout=3600" --class org.vitrivr.adampro.main.Startup $ADAMPRO_HOME/ADAMpro-assembly-0.1.0.jar &
