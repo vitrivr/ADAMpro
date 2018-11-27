cd5b61b134f94baffbb039edddd9aa4322f2f451
Ivan Giangreco <ivan.giangreco@unibas.ch>
2018-11-27 13:28:12 +0100

    fix error in solr config

--- src/main/scala/org/vitrivr/adampro/storage/engine/SolrEngine.scala
+++ src/main/scala/org/vitrivr/adampro/storage/engine/SolrEngine.scala
@@ -75,7 +75,7 @@ class SolrEngine(private val url: String)(@transient override implicit val ac: S
       val createReq = new CoreAdminRequest.Create()
       createReq.setCoreName(storename)
       createReq.setInstanceDir(storename)
-      createReq.setConfigSet("_default")
+      createReq.setConfigSet("basic_configs")
       createReq.process(client)
 
       Success(Map())
